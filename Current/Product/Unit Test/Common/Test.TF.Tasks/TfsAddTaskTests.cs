17d4290ff5ac8c9d3d284685c5f48214d6b0f170
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-01-26 15:30:10 +0000

    The initial commit for TF.Task tests.

---
+++ Current/Product/Unit Test/Common/Test.TF.Tasks/TfsAddTaskTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MbUnit.Framework;
+
+namespace Test.TF.Tasks
+{
+    [TestFixture]
+    public class TfsAddTaskTests
+    {
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+
+}
