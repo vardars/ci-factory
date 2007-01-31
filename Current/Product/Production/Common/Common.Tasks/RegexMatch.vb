09fc9ce2ca90be3ed25cf082af2bea1fc06c69b6
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-01-31 03:07:56 +0000

    Switch to only .NET 2.0 Implemented RegexMatch Task.

--- Current/Product/Production/Common/Common.Tasks/RegexMatch.vb
+++ Current/Product/Production/Common/Common.Tasks/RegexMatch.vb
@@ -1,16 +1,31 @@
 Option Explicit On
 Option Strict On
 
+Imports System.Collections
+Imports System.Collections.Generic
 Imports System.IO
 Imports System.Text
 Imports NAnt.Core
 Imports NAnt.Core.Attributes
+Imports System
+Imports System.Text.RegularExpressions
 
 <ElementName("regexmatch")> _
 Public Class RegexMatch
     Inherits LoopItems
 
     Private _File As FileInfo
+    Private _Pattern As String
+
+    <TaskAttribute("pattern", Required:=False)> _
+    Public Property Pattern() As String
+        Get
+            Return _Pattern
+        End Get
+        Set(ByVal value As String)
+            _Pattern = value
+        End Set
+    End Property
 
     <TaskAttribute("file", Required:=True)> _
     Public Property File() As FileInfo
@@ -22,11 +37,86 @@ Public Class RegexMatch
         End Set
     End Property
 
-    Protected Overrides Function GetStrings() As System.Collections.IEnumerator
-        Dim Strings As New ArrayList
-
-
-
-        Return DirectCast(Strings.ToArray(GetType(String)), String()).GetEnumerator
+    Protected Overrides Function GetStrings() As IEnumerator
+        Dim Enumerator As New LineEnumerator(Me.File.OpenText(), New Regex(Me.Pattern))
+        Return Enumerator
     End Function
+End Class
+
+Public Class LineEnumerator
+    Implements IEnumerator
+#Region "Fields"
+
+    Private _Current As String
+    Private _reader As TextReader
+    Private _regularEpression As Regex
+
+#End Region
+
+#Region "Properties"
+
+    Public Property Reader() As TextReader
+        Get
+            Return _reader
+        End Get
+        Set(ByVal Value As TextReader)
+            _reader = Value
+        End Set
+    End Property
+
+    Public Property RegularEpression() As Regex
+        Get
+            Return _regularEpression
+        End Get
+        Set(ByVal Value As Regex)
+            _regularEpression = Value
+        End Set
+    End Property
+
+#End Region
+
+#Region "Constructors"
+
+    Public Sub New(ByVal reader As TextReader, ByVal regularEpression As Regex)
+        _reader = reader
+        _regularEpression = regularEpression
+    End Sub
+
+#End Region
+
+    Public ReadOnly Property Current() As Object Implements IEnumerator.Current
+        Get
+            Return Me._Current
+        End Get
+    End Property
+
+    Public Function GetNextLine() As String
+        Dim Line As String
+        While True
+            Line = Me.Reader.ReadLine()
+
+            If Line Is Nothing Then
+                Return Nothing
+            End If
+
+            If Me.RegularEpression.IsMatch(Line) Then
+                Return Line
+            End If
+        End While
+        Return Nothing
+    End Function
+
+    Public Function MoveNext() As Boolean Implements IEnumerator.MoveNext
+        Me._Current = Me.GetNextLine()
+        If Me._Current Is Nothing Then
+            Return False
+            Me.Reader.Close()
+        End If
+        Return True
+    End Function
+
+    Public Sub Reset() Implements IEnumerator.Reset
+        Throw New NotSupportedException("Reset is not suppoerted")
+    End Sub
+
 End Class
--- Current/Product/Production/Common/Common.Tasks/UpdatenAnt.bat
+++ Current/Product/Production/Common/Common.Tasks/UpdatenAnt.bat
@@ -1,5 +1,5 @@
 attrib -R "C:\Projects\CI Factory\Current\Third Party\nAnt\bin\Common.Tasks.dll"
 
-copy "bin\.NET 1.1\Debug\Common.Tasks.dll" "C:\Projects\CI Factory\Current\Third Party\nAnt\bin"
-copy "bin\.NET 1.1\Debug\Common.Tasks.pdb" "C:\Projects\CI Factory\Current\Third Party\nAnt\bin"
+copy "bin\Common.Tasks.dll" "C:\Projects\CI Factory\Current\Third Party\nAnt\bin"
+copy "bin\Common.Tasks.pdb" "C:\Projects\CI Factory\Current\Third Party\nAnt\bin"
 nant -buildfile:"C:\Projects\CI Factory\Current\Product\DevEnv\SetupScripts\SetUp.xml"
