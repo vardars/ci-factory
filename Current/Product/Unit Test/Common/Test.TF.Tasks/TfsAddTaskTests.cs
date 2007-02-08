c1fa9a79059c8463314130d1d6da19ded9d6a03f
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-02-08 18:37:29 +0000

    Reorganize in anticipation of absorption of CCNet source

--- Current/Product/Unit Test/Common/Test.TF.Tasks/TfsAddTaskTests.cs
+++ Current/Product/Unit Test/Common/Test.TF.Tasks/TfsAddTaskTests.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using MbUnit.Framework;
 
+using TF.Tasks.SourceControl;
+
 namespace Test.TF.Tasks
 {
     [TestFixture]
     public class TfsAddTaskTests
     {
         [Test]
-        public void Test()
+        public void TestAddNewFile()
         {
 
         }
