ae5352e259c73b42c8d6d818742dd86503cd4415
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-02-20 21:39:36 +0000

    Shuffle Package for new package structure.

---
+++ Current/Product/Production/Nant/CIFactory.NAnt.Tracker.Tasks/Source/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tracker.Tasks")]
+[assembly: AssemblyDescription("nAnt tasks for manipulating PVCS Trackers")]
+[assembly: ComVisible(false)]
+[assembly: Guid("13e7f34b-09c6-4e64-992b-1cb875adfcc3")]
+[assembly: AssemblyVersion("1.0.0.1")]
