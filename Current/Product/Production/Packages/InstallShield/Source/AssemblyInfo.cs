e82da81c4c2009fc7aa38ece82a730db5e84966a
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-02-20 18:29:33 +0000

    Shuffle Package for new package structure.

--- Current/Product/Production/Packages/InstallShield/Source/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("InstallShield.Tasks")]
-[assembly: AssemblyDescription("nAnt tasks for compiling InstallShield projects")]
-[assembly: ComVisible(false)]
-[assembly: Guid("b8232f9c-2080-4e1c-adcb-e276c08ff0dd")]
-[assembly: AssemblyVersion("1.0.0.1")]
