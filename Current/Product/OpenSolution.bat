7c529d526d64de0dc627a39a4623866fe93f9c65
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-02-22 20:56:32 +0000

    Update MbUnit to 2.3 RTM. Change OpenSolution script to set registry keys for TD.Net to use the MbUnit installation in the Third Party Directory.

--- Current/Product/OpenSolution.bat
+++ Current/Product/OpenSolution.bat
@@ -1,3 +1,4 @@
 set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;C:\Program Files\PVCS\Tracker\nt;C:\Program Files\Subversion\bin;C:\Projects\CI Factory\Current\Third Party\nAnt\bin
 call "C:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat"
+nant -buildfile:OpenSolution.Script.xml
 devenv "CI Factory.sln"
