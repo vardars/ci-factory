ea04bcb9f1e938fbe60309ecbde7ac56a4563da7
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-02-26 15:34:19 +0000

    Added templates and images dirs to the dashboard output. Added cctray zip deployment script.

--- Current/Product/Production/CCNet/WebDashboard/UpdateFoundation.bat
+++ Current/Product/Production/CCNet/WebDashboard/UpdateFoundation.bat
@@ -1,6 +1,13 @@
 :: "$(ProjectDir)UpdateFoundation.bat" "$(SolutionDir)" "$(TargetDir)"
 cd %2
 xcopy * %1Production\Foundation\Default\Build\dashboard /S /Y
+
+mkdir %1Production\Foundation\Default\Build\dashboard\templates
+xcopy ..\..\templates %1Production\Foundation\Default\Build\dashboard\templates /S /Y
+
+mkdir %1Production\Foundation\Default\Build\dashboard\images
+xcopy ..\..\images %1Production\Foundation\Default\Build\dashboard\images /S /Y
+
 mkdir %1Production\Foundation\Default\Build\dashboard\bin
 move /Y %1Production\Foundation\Default\Build\dashboard\*.dll %1Production\Foundation\Default\Build\dashboard\bin
 move /Y %1Production\Foundation\Default\Build\dashboard\*.pdb %1Production\Foundation\Default\Build\dashboard\bin
