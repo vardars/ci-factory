cb745c069a9cc65974a996e1a9b6e5b07222fc45
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-01 19:30:47 +0000

    CCNET build 0.8.0.14

---
+++ Product/Production/CCNet/Remote/ICruiseServer.cs
@@ -0,0 +1,94 @@
+using System;
+
+namespace ThoughtWorks.CruiseControl.Remote
+{
+	public interface ICruiseServer : IDisposable
+	{
+		/// <summary>
+		/// Launches the CruiseControl.NET server and starts all project schedules it contains
+		/// </summary>
+		void Start();
+
+		/// <summary>
+		/// Requests all started projects within the CruiseControl.NET server to stop
+		/// </summary>
+		void Stop();
+
+		/// <summary>
+		/// Terminates the CruiseControl.NET server immediately, stopping all started projects
+		/// </summary>
+		void Abort();
+
+		/// <summary>
+		/// Wait for CruiseControl server to finish executing
+		/// </summary>
+		void WaitForExit();
+
+		/// <summary>
+		/// Retrieve CruiseManager interface for the server
+		/// </summary>
+		ICruiseManager CruiseManager { get; }
+
+		/// <summary>
+		/// Gets information about the last build status, current activity and project name.
+		/// for all projects on a cruise server
+		/// </summary>
+		ProjectStatus [] GetProjectStatus();
+
+		bool ForceBuild(string projectName, ForceFilterClientInfo[] clientInfo);
+
+		void WaitForExit(string projectName);
+		
+		/// <summary>
+		/// Returns the name of the most recent build for the specified project
+		/// </summary>
+		string GetLatestBuildName(string projectName);
+
+		/// <summary>
+		/// Returns the names of all builds for the specified project, sorted s.t. the newest build is first in the array
+		/// </summary>
+		string[] GetBuildNames(string projectName);
+
+		/// <summary>
+		/// Returns the names of the buildCount most recent builds for the specified project, sorted s.t. the newest build is first in the array
+		/// </summary>
+		string[] GetMostRecentBuildNames(string projectName, int buildCount);
+
+		/// <summary>
+		/// Returns the build log contents for requested project and build name
+		/// </summary>
+		string GetLog(string projectName, string buildName);
+
+		/// <summary>
+		/// Returns a log of recent build server activity. How much information that is returned is configured on the build server.
+		/// </summary>
+		string GetServerLog();
+
+		/// <summary>
+		/// Returns the version number of the server
+		/// </summary>
+		string GetVersion();
+
+		/// <summary>
+		/// Adds a project to the server
+		/// </summary>
+		void AddProject(string serializedProject);
+
+		/// <summary>
+		/// Deletes the specified project from the server
+		/// </summary>
+		void DeleteProject(string projectName, bool purgeWorkingDirectory, bool purgeArtifactDirectory, bool purgeSourceControlEnvironment);
+
+		/// <summary>
+		/// Returns the serialized form of the requested project from the server
+		/// </summary>
+		string GetProject(string name);
+
+		/// <summary>
+		/// Updates the specified project configuration on the server
+		/// </summary>
+		void UpdateProject(string projectName, string serializedProject);
+
+		ExternalLink[] GetExternalLinks(string projectName);
+	}
+}
---
+++ Product/Production/CCNet/core/label/IterationLabeller.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ThoughtWorks.CruiseControl.Core.Label
+{
+	/// <summary>
+	/// The IterationLabeller increments the build number automatically after each iteration, where an iteration is defined as a 
+	/// configurable number of weeks (the default is 2).
+	/// </summary>
+	[ReflectorType("iterationlabeller")]
+	public class IterationLabeller : ILabeller
+	{
+		private readonly DateTimeProvider dateTimeProvider;
+		public const int InitialLabel = 1;
+		private const int DaysInWeek = 7;
+
+		public IterationLabeller() : this(new DateTimeProvider())
+		{}
+
+		public IterationLabeller(DateTimeProvider dateTimeProvider)
+		{
+			this.dateTimeProvider = dateTimeProvider;
+		}
+
+		[ReflectorProperty("prefix", Required=false)]
+		public string LabelPrefix = "";
+
+		/// <summary>
+		/// Duration of the interation measured in weeks,
+		/// default is 2.
+		/// </summary>
+		[ReflectorProperty("duration", Required=false)]
+		public int Duration = 2;
+
+		[ReflectorProperty("releaseStartDate")]
+		public DateTime ReleaseStartDate;
+
+		[ReflectorProperty("separator", Required=false)]
+		public string Separator = ".";
+
+		/// <summary>
+		/// Runs the task, given the specified <see cref="IIntegrationResult"/>, in the specified <see cref="IProject"/>.
+		/// </summary>
+		/// <param name="result"></param>
+		public void Run(IIntegrationResult result)
+		{
+			result.Label = Generate(result);
+		}
+
+		public string Generate(IIntegrationResult previousResult)
+		{
+			if (previousResult == null || previousResult.Label == null || previousResult.IsInitial())
+			{
+				return NewLabel(InitialLabel);
+			}
+			else if (previousResult.Status == IntegrationStatus.Success)
+			{
+				return NewLabel(IncrementLabel(previousResult.Label));
+			}
+			else
+			{
+				return previousResult.Label;
+			}
+		}
+
+		private string NewLabel(int suffix)
+		{
+			StringBuilder buffer = new StringBuilder();
+			buffer.Append(LabelPrefix);
+			if (LabelPrefix != string.Empty && ! LabelPrefix.EndsWith(Separator)) buffer.Append(Separator);
+			buffer.Append(CurrentIteration());
+			buffer.Append(Separator);
+			buffer.Append(suffix);
+			return buffer.ToString();
+		}
+
+		private int IncrementLabel(string label)
+		{
+			string iterationPtn = @".*?((\d+)" + Separator.Replace(".", @"\.") + "(\\d+$)).*";
+			string iterationLabel = Regex.Replace(label, iterationPtn, "$2");
+			int numericIteration = int.Parse(iterationLabel);
+			if (numericIteration < CurrentIteration())
+			{
+				return InitialLabel;
+			}
+			else
+			{
+				string numericLabel = Regex.Replace(label, @".*?(\d+$)", "$1");
+				int newLabel = int.Parse(numericLabel);
+				return newLabel + 1;
+			}
+		}
+
+		private int CurrentIteration()
+		{
+			return GetIteration(ReleaseStartDate);
+		}
+
+		private int GetIteration(DateTime startDate)
+		{
+			return GetIteration(startDate, dateTimeProvider.Today);
+		}
+
+		private int GetIteration(DateTime startDate, DateTime endDate)
+		{
+			double daysFromStart = (endDate - startDate).TotalDays;
+			return (int) daysFromStart/(Duration*DaysInWeek);
+		}
+	}
+}
