331e59b96a727e7be6dd006fce705aae595249a1
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-02 18:21:43 +0000

    Moved the svncleantree task to a new project just for the Subversion Package. Improved the Subversion Gets and CleanGets. Allow the Simian Package to fail the build. Turn off verbose on Simian, MSTest, and NCover Packages.

--- Current/Product/Production/Nant/CIFactory.NAnt.Tasks/UpdatenAnt.bat
+++ Current/Product/Production/Nant/CIFactory.NAnt.Tasks/UpdatenAnt.bat
@@ -9,13 +9,4 @@ copy %3.dll "%Dest%"
 copy %3.pdb "%Dest%"
 
 cd %1
-cd ..\Third Party\Svn.Net
-copy *.dll "%Dest%"
-copy *.pdb "%Dest%"
-
-cd %1
-cd ..\Third Party\That
-copy *.dll "%Dest%"
-
-cd %1
 nant -buildfile:DevEnv\SetupScripts\SetUp.xml
