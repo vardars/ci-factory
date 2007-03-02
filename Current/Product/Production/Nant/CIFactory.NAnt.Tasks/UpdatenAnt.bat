63708db41cc2ea34366a295ccb6e7b90658c49eb
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-02 04:20:02 +0000

    Added new task svncleantree. Changed the CleanGet... tasks in the Svn Package to use the svncleantree task.

--- Current/Product/Production/Nant/CIFactory.NAnt.Tasks/UpdatenAnt.bat
+++ Current/Product/Production/Nant/CIFactory.NAnt.Tasks/UpdatenAnt.bat
@@ -7,5 +7,15 @@ FOR /F "TOKENS=1 DELIMS=," %%A IN ('cd') DO SET Dest=%%A
 cd %2
 copy %3.dll "%Dest%"
 copy %3.pdb "%Dest%"
+
+cd %1
+cd ..\Third Party\Svn.Net
+copy *.dll "%Dest%"
+copy *.pdb "%Dest%"
+
+cd %1
+cd ..\Third Party\That
+copy *.dll "%Dest%"
+
 cd %1
 nant -buildfile:DevEnv\SetupScripts\SetUp.xml
