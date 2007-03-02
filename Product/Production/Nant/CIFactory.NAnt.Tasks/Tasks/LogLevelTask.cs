5830ea233ea5dc98d8f4607220fa8327623f1d05
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-02 21:29:31 +0000

    CCNET build 0.8.0.22

---
+++ Product/Production/Nant/CIFactory.NAnt.Tasks/Tasks/LogLevelTask.cs
@@ -0,0 +1,62 @@
+using System;
+using NAnt.Core;
+using NAnt.Core.Attributes;
+
+namespace CIFactory.NAnt.Tasks
+{
+    [TaskName("loglevel")]
+    public class LogLevelTask : Task
+    {
+        #region Fields
+
+        private Level _LogLevel;
+
+        private TaskContainer _Tasks;
+
+        #endregion
+
+        #region Properties
+
+        [TaskAttribute("level", Required = true)]
+        public Level LogLevel
+        {
+            get
+            {
+                return _LogLevel;
+            }
+            set
+            {
+                _LogLevel = value;
+            }
+        }
+
+        [BuildElement("do", Required = true)]
+        public TaskContainer Tasks
+        {
+            get
+            {
+                return _Tasks;
+            }
+            set
+            {
+                _Tasks = value;
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void ExecuteTask()
+        {
+            Level OldLevel = this.Project.Threshold;
+            this.Project.Threshold = this.LogLevel;
+            this.Tasks.Execute();
+            this.Project.Threshold = OldLevel;
+
+        }
+
+        #endregion
+
+    }
+}
