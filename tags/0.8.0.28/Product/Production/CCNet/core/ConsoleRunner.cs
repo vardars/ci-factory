c118d288681519851b7b02493c9e1da8fc488b61
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-07 21:16:02 +0000

    CCNET build 0.8.0.28

---
+++ tags/0.8.0.28/Product/Production/CCNet/core/ConsoleRunner.cs
@@ -0,0 +1,56 @@
+using System;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ThoughtWorks.CruiseControl.Core
+{
+	public class ConsoleRunner
+	{
+		private readonly ArgumentParser _parser;
+		private readonly ICruiseServerFactory _serverFactory;
+		private ICruiseServer server;
+
+		public ConsoleRunner(ArgumentParser parser, ICruiseServerFactory serverFactory)
+		{
+			_parser = parser;
+			_serverFactory = serverFactory;
+		}
+
+		public void Run()
+		{
+			if (_parser.ShowHelp)
+			{
+				Log.Warning(ArgumentParser.Usage);
+				return;
+			}
+			LaunchServer();
+		}
+
+		private void LaunchServer()
+		{
+			using (ConsoleEventHandler handler = new ConsoleEventHandler())
+			{
+				handler.OnConsoleEvent += new EventHandler(HandleControlEvent);
+
+				using (server = _serverFactory.Create(_parser.UseRemoting, _parser.ConfigFile))
+				{
+					if (_parser.Project == null)
+					{
+						server.Start();
+						server.WaitForExit();
+					}
+					else
+					{
+						server.ForceBuild(_parser.Project, null);
+						server.WaitForExit(_parser.Project);
+					}
+				}
+			}
+		}
+
+		private void HandleControlEvent(object sender, EventArgs args)
+		{
+			server.Dispose();
+		}
+	}
+}
