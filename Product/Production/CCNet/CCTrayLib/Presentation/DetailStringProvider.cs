19263b5c76ba6e9e5bc388a610f7e9e4e042e4dd
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-08 18:43:00 +0000

    CCNET build 0.8.0.31

---
+++ Product/Production/CCNet/CCTrayLib/Presentation/DetailStringProvider.cs
@@ -0,0 +1,42 @@
+using System;
+using ThoughtWorks.CruiseControl.CCTrayLib.Monitoring;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ThoughtWorks.CruiseControl.CCTrayLib.Presentation
+{
+	public class DetailStringProvider : IDetailStringProvider
+	{
+		public string FormatDetailString(ISingleProjectDetail projectStatus)
+		{
+			if (projectStatus.ProjectState == ProjectState.NotConnected)
+			{
+				if (projectStatus.ConnectException == null)
+					return "Connecting...";
+
+				return string.Format("Error: {0}", projectStatus.ConnectException.Message);
+			}
+
+			if (projectStatus.Activity == ProjectActivity.Sleeping)
+			{
+				if (projectStatus.NextBuildTime == DateTime.MaxValue)
+					return "Project is not automatically triggered";
+				
+				return string.Format("Next build check: {0:T}", projectStatus.NextBuildTime);
+			}
+			
+			TimeSpan durationRemaining = projectStatus.EstimatedTimeRemainingOnCurrentBuild;
+			
+			if (durationRemaining != TimeSpan.MaxValue)
+			{
+				if (durationRemaining <= TimeSpan.Zero)
+				{
+					return string.Format("Taking {0} longer than last build", new CCTimeFormatter(durationRemaining.Negate()));
+				}
+				
+				return string.Format("{0} estimated remaining", new CCTimeFormatter(durationRemaining));
+			}
+			
+			return string.Empty;
+		}
+	}
+}
