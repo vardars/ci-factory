b04682b8cc6fbbd46b0b0c98a37423c1af92455c
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-15 00:47:26 +0000

    CCNET build 0.8.0.66

---
+++ tags/0.8.0.66/Product/Production/CCNet/WebDashboard/IO/ICruiseRequest.cs
@@ -0,0 +1,19 @@
+using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
+using ThoughtWorks.CruiseControl.WebDashboard.Dashboard;
+using ThoughtWorks.CruiseControl.WebDashboard.MVC;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.IO
+{
+	public interface ICruiseRequest
+	{
+		string ServerName { get; }
+		string ProjectName { get; }
+		string BuildName { get; }
+
+		IServerSpecifier ServerSpecifier { get;  }
+		IProjectSpecifier ProjectSpecifier { get;  }
+		IBuildSpecifier BuildSpecifier { get;  }
+
+		IRequest Request { get; }
+	}
+}
