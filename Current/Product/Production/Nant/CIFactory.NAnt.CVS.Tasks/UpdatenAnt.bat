afd2d21d49339f0db07e5f4741fed04d18fdbb82
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-17 18:14:23 +0000

    Tried to the unit tests for the CVS stuff.  Can't tell if there is an issue with SourceForge or the tests.

--- Current/Product/Production/Nant/CIFactory.NAnt.CVS.Tasks/UpdatenAnt.bat
+++ Current/Product/Production/Nant/CIFactory.NAnt.CVS.Tasks/UpdatenAnt.bat
@@ -3,5 +3,11 @@ mkdir %1Production\Packages\CVS\bin
 copy %2%3.dll %1Production\Packages\CVS\bin
 copy %2%3.pdb %1Production\Packages\CVS\bin
 copy %2ICSharpCode.SharpCvsLib.dll %1Production\Packages\CVS\bin
+copy %2ICSharpCode.SharpZipLib.dll %1Production\Packages\CVS\bin
+
+copy "..\..\..\..\..\..\Third Party\ICSharpCode\ICSharpCode.SharpCvsLib.Console.dll" %1Production\Packages\CVS\bin
+copy "..\..\..\..\..\..\Third Party\ICSharpCode\log4net.dll" %1Production\Packages\CVS\bin
+copy "..\..\..\..\..\..\Third Party\ICSharpCode\scvs.exe" %1Production\Packages\CVS\bin
+
 cd %1
 nant -buildfile:DevEnv\SetupScripts\SetUp.xml
