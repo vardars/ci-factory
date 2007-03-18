f521de637eccec18218f8d5d3b6ef2bcccbfd562
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-18 16:45:30 +0000

    CCNET build 0.8.0.82

---
+++ Product/Production/CCNet/WebDashboard/Plugins/ProjectReport/ProjectReportProjectPlugin.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
+using ThoughtWorks.CruiseControl.WebDashboard.Dashboard;
+using ThoughtWorks.CruiseControl.WebDashboard.IO;
+using ThoughtWorks.CruiseControl.WebDashboard.MVC;
+using ThoughtWorks.CruiseControl.WebDashboard.MVC.Cruise;
+using ThoughtWorks.CruiseControl.WebDashboard.MVC.View;
+using ThoughtWorks.CruiseControl.WebDashboard.Plugins.BuildReport;
+using ThoughtWorks.CruiseControl.WebDashboard.ServerConnection;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.Plugins.ProjectReport
+{
+	[ReflectorType("projectReportProjectPlugin")]
+	public class ProjectReportProjectPlugin : ICruiseAction, IPlugin
+	{
+		private readonly IFarmService farmService;
+		private readonly IVelocityViewGenerator viewGenerator;
+		private readonly ILinkFactory linkFactory;
+		public static readonly string ACTION_NAME = "ViewProjectReport";
+
+		public ProjectReportProjectPlugin(IFarmService farmService, IVelocityViewGenerator viewGenerator, ILinkFactory linkFactory)
+		{
+			this.farmService = farmService;
+			this.viewGenerator = viewGenerator;
+			this.linkFactory = linkFactory;
+		}
+
+		public IResponse Execute(ICruiseRequest cruiseRequest)
+		{
+			Hashtable velocityContext = new Hashtable();
+			IProjectSpecifier projectSpecifier = cruiseRequest.ProjectSpecifier;
+
+			IBuildSpecifier[] buildSpecifiers = farmService.GetMostRecentBuildSpecifiers(projectSpecifier, 1);
+			if (buildSpecifiers.Length == 1)
+			{
+				velocityContext["mostRecentBuildUrl"] = linkFactory.CreateProjectLink(projectSpecifier, LatestBuildReportProjectPlugin.ACTION_NAME).Url;
+			}
+
+			velocityContext["projectName"] = projectSpecifier.ProjectName;
+			velocityContext["externalLinks"] = farmService.GetExternalLinks(projectSpecifier);
+			velocityContext["noLogsAvailable"] = (buildSpecifiers.Length == 0);
+
+			return viewGenerator.GenerateView(@"ProjectReport.vm", velocityContext);
+		}
+
+		public string LinkDescription
+		{
+			get { return "Project Report"; }
+		}
+
+		public INamedAction[] NamedActions
+		{
+			get {  return new INamedAction[] { new ImmutableNamedAction(ACTION_NAME, this) }; }
+		}
+	}
+}
