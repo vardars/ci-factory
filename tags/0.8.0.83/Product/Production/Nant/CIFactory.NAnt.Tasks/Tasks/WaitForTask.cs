446ec213ceebb6a1d747e5032a6e47cd4f345f5c
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-18 16:52:58 +0000

    CCNET build 0.8.0.83

---
+++ tags/0.8.0.83/Product/Production/Nant/CIFactory.NAnt.Tasks/Tasks/WaitForTask.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Threading;
+using NAnt.Core;
+using NAnt.Core.Attributes;
+
+namespace CIFactory.NAnt.Tasks
+{
+    [TaskName("waitfor")]
+    public class WaitForTask : Task
+    {
+        private string _Condition;
+        private int _timeOutInSeconds = 5;
+        private int _pollingIntervalInSeconds = 5;
+
+        [TaskAttribute("condition", ExpandProperties = false, Required = true)]
+        public string Condition
+        {
+            get
+            {
+                return _Condition;
+            }
+            set
+            {
+                _Condition = value;
+            }
+        }
+
+        [TaskAttribute("timeout", Required = false)]
+        public int TimeOut
+        {
+            get { return _timeOutInSeconds; }
+            set { _timeOutInSeconds = value; }
+        }
+
+        [TaskAttribute("pollinginterval", Required = false)]
+        public int PollingInterval
+        {
+            get { return _pollingIntervalInSeconds; }
+            set { _pollingIntervalInSeconds = value; }
+        }
+
+        protected override void ExecuteTask()
+        {
+            if (bool.Parse(this.Project.ExpandProperties(this.Condition, this.Location)))
+                return;
+            DateTime startTime = DateTime.Now;
+            TimeSpan timeout = new TimeSpan(0, 0, TimeOut);
+            while (true)
+            {
+                TimeSpan elapsed = DateTime.Now - startTime;
+                if (elapsed >= timeout)
+                    throw new BuildException("Timed-out");
+                Thread.Sleep(PollingInterval * 1000);
+
+                if (bool.Parse(this.Project.ExpandProperties(this.Condition, this.Location)))
+                    break;
+            }
+        }
+    }
+}
