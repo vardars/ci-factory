1e905552939e9d8ecd096608af689ae5519fd322
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-18 19:53:43 +0000

    CCNET build 0.8.0.85

---
+++ Product/Production/Nant/CIFactory.NAnt.Tasks/Functions/VSProjectFunctions.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using NAnt.Core;
+using NAnt.Core.Attributes;
+
+namespace CIFactory.NAnt.Functions
+{
+    [FunctionSet("vsproject", "visualstudio")]
+    public class VSProjectFunctions : FunctionSetBase
+    {
+
+        #region Constructors
+
+        public VSProjectFunctions(Project project, PropertyDictionary properties)
+            : base(project, properties)
+        {
+        }
+
+        public VSProjectFunctions()
+            : base(null, null)
+        {
+        }
+
+        #endregion
+
+        [Function("get-assemblyname")]
+        public string GetSssemblyName(string projectFilePath)
+        {
+            XmlDocument xd = new XmlDocument();
+            xd.PreserveWhitespace = true;
+            xd.Load(projectFilePath);
+
+            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(xd.NameTable);
+            namespaceManager.AddNamespace("b", @"http://schemas.microsoft.com/developer/msbuild/2003");
+
+            XmlNode Node = null;
+            Node = xd.SelectSingleNode("//b:AssemblyName", namespaceManager);
+            return Node.InnerText;
+        }
+
+        [Function("get-output-directory")]
+        public string GetOutputDirectory(string projectFilePath, string config)
+        {
+            XmlDocument xd = new XmlDocument();
+            xd.PreserveWhitespace = true;
+            xd.Load(projectFilePath);
+
+            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(xd.NameTable);
+            namespaceManager.AddNamespace("b", @"http://schemas.microsoft.com/developer/msbuild/2003");
+
+            XmlNode Node = null;
+            Node = xd.SelectSingleNode(string.Format("//b:PropertyGroup[contains(@Condition, {0})]/b:OutputPath", config), namespaceManager);
+            string OutputValue = Node.InnerText;
+            
+            if (Path.IsPathRooted(OutputValue))
+                return OutputValue;
+
+            return Path.GetFullPath(Path.Combine(Path.GetDirectoryName(projectFilePath), OutputValue));
+        }
+
+    }
+}
