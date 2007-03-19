8aba1784565d9aa51975135254c350fec4adbd9d
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-19 00:33:57 +0000

    Make the bin dir first!

--- Current/Product/Production/CCNet/CCNET.Extensions.Plugin/UpdateFoundation.bat
+++ Current/Product/Production/CCNet/CCNET.Extensions.Plugin/UpdateFoundation.bat
@@ -1,4 +1,5 @@
 :: "$(ProjectDir)UpdateFoundation.bat" "$(SolutionDir)" "$(TargetDir)"
+mkdir %1Production\Foundation\Default\Build\dashboard\bin
 copy %2%3.dll %1Production\Foundation\Default\Build\dashboard\bin
 copy %2%3.pdb %1Production\Foundation\Default\Build\dashboard\bin
 
