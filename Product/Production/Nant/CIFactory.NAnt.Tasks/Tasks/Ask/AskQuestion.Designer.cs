0a503d1634b6c86b6fad4ac0f2a90ba76dbed958
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-03-30 02:17:03 +0000

    CCNET build 0.8.0.111

---
+++ Product/Production/Nant/CIFactory.NAnt.Tasks/Tasks/Ask/AskQuestion.Designer.cs
@@ -0,0 +1,220 @@
+namespace CIFactory.NAnt.Tasks.Ask
+{
+    partial class AskQuestion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(603, 360);
+            this.splitContainer1.SplitterDistance = 225;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.CausesValidation = false;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(603, 225);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(603, 131);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choices";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(284, 80);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(85, 17);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(451, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 40);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Choose";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(284, 56);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(85, 17);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.Visible = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(284, 29);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Visible = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(43, 80);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Visible = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(43, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(43, 29);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            // 
+            // AskQuestion
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 360);
+            this.ControlBox = false;
+            this.Controls.Add(this.splitContainer1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AskQuestion";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "AskQuestion";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+
+    }
+}
---
+++ Product/Production/Nant/CIFactory.NAnt.Tasks/Tasks/AskTask.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NAnt.Core;
+using NAnt.Core.Attributes;
+using CIFactory.NAnt.Types;
+
+namespace CIFactory.NAnt.Tasks
+{
+    [TaskName("ask")]
+    public class AskTask : Task
+    {
+        #region Fields
+
+        private string _AnswerProperty;
+        private StringList _Options;
+        private string _Question;
+        private bool _ShowDialog = true;
+        private string _Caption;
+
+        #endregion
+
+        #region Properties
+
+
+        [TaskAttribute("caption", Required = false)]
+        public string Caption
+        {
+            get
+            {
+                return _Caption;
+            }
+            set
+            {
+                _Caption = value;
+            }
+        }
+
+        [TaskAttribute("showdialog", Required = false), BooleanValidator()]
+        public bool ShowDialog
+        {
+            get
+            {
+                return _ShowDialog;
+            }
+            set
+            {
+                _ShowDialog = value;
+            }
+        }
+
+        [BuildElement("options", Required = false)]
+        public StringList Options
+        {
+            get { return _Options; }
+            set { _Options = value; }
+        }
+
+        [TaskAttribute("answer", Required = true)]
+        public string AnswerProperty
+        {
+            get
+            {
+                return _AnswerProperty;
+            }
+            set
+            {
+                _AnswerProperty = value;
+            }
+        }
+
+        [TaskAttribute("question", Required = true)]
+        public string Question
+        {
+            get
+            {
+                return _Question;
+            }
+            set
+            {
+                _Question = value;
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void ExecuteTask()
+        {
+            string Answer;
+            if (this.ShowDialog)
+            {
+                Ask.AskQuestion Questioner = new CIFactory.NAnt.Tasks.Ask.AskQuestion();
+                foreach (String Choice in this.Options)
+                {
+                    Questioner.AddChoice(Choice);
+                }
+                Questioner.StateQuestion(this.Question);
+                if (!String.IsNullOrEmpty(this.Caption))
+                    Questioner.SetCaption(this.Caption);
+                Questioner.ShowDialog();
+                Answer = Questioner.GetAnswer();
+            }
+            else
+            {
+                Log(Level.Info, this.Question);
+                Answer = Console.ReadLine();
+            }
+            this.Properties[this.AnswerProperty] = Answer;
+        }
+
+        #endregion
+
+    }
+
+    internal class Adhoctest
+    {
+        public void Test()
+        {
+            Ask.AskQuestion Questioner = new CIFactory.NAnt.Tasks.Ask.AskQuestion();
+            Questioner.AddChoice("Install");
+            Questioner.AddChoice("Don't Install");
+            Questioner.AddChoice("Quit");
+            Questioner.StateQuestion(string.Format("{0}{1}{0}", "I really have a lot to say.", Environment.NewLine));
+            Questioner.ShowDialog();
+            string Answer = Questioner.GetAnswer();
+            Console.WriteLine(Answer);
+        }
+    }
+}
