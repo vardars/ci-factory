f129613f95263a66d1f75c6c1777992e6d22dc09
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-04-02 15:22:02 +0000

    CCNET build 0.8.0.122

---
+++ Product/Production/CCNet/CCTrayLib/Configuration/PersistentConfiguration.cs
@@ -0,0 +1,15 @@
+using System.Xml.Serialization;
+
+namespace ThoughtWorks.CruiseControl.CCTrayLib.Configuration
+{
+	[XmlRoot(Namespace="", IsNullable=false, ElementName="Configuration")]
+	public class PersistentConfiguration
+	{
+		public Project[] Projects = new Project[0];
+		public int PollPeriodSeconds = 5;
+		public BuildTransitionNotification BuildTransitionNotification = new BuildTransitionNotification();
+		public TrayIconDoubleClickAction TrayIconDoubleClickAction = TrayIconDoubleClickAction.ShowStatusWindow;
+		public Icons Icons = new Icons();
+	}
+
+}
