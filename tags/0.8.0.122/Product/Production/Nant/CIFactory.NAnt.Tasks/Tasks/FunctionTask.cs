0508c15fc58b3fe909c608fac54b0fe069cdd707
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-04-02 15:22:02 +0000

    CCNET build 0.8.0.122

---
+++ tags/0.8.0.122/Product/Production/Nant/CIFactory.NAnt.Tasks/Tasks/FunctionTask.cs
@@ -0,0 +1,34 @@
+using System;
+using NAnt.Core;
+using NAnt.Core.Attributes;
+
+namespace CIFactory.NAnt.Tasks
+{
+
+    [TaskName("function")]
+    public class FunctionTask : Task
+    {
+        #region Properties
+
+        [TaskAttribute("execute", ExpandProperties = true, Required = true)]
+        public String Execute
+        {
+            set
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void ExecuteTask()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
