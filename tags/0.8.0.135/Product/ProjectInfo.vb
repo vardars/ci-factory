1bf28ed313d552e849995417b0ea5805368273d2
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-05-15 01:26:36 +0000

    CCNET build 0.8.0.135

---
+++ tags/0.8.0.135/Product/ProjectInfo.vb
@@ -0,0 +1,7 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyProduct("CI Factory")> 
+<Assembly: AssemblyInformationalVersion("0.8.0.134")> 
+<Assembly: AssemblyCompany("http://www.cifactory.org")> 
