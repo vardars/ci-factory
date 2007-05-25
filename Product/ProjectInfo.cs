41b0267c333af98ca9259f1301c5b008ecee9589
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-05-25 01:25:08 +0000

    CCNET build 0.8.0.145

---
+++ Product/ProjectInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("CI Factory")]
+[assembly: AssemblyInformationalVersion("0.8.0.144")]
+[assembly: AssemblyCompany("http://www.cifactory.org")]
