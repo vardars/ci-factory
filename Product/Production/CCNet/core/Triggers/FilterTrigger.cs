47f82508900451eb3f78918cc065f5683fab4bcd
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-06-10 19:30:26 +0000

    CCNET build 0.8.0.154

---
+++ Product/Production/CCNet/core/Triggers/FilterTrigger.cs
@@ -0,0 +1,116 @@
+using System;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ThoughtWorks.CruiseControl.Core.Triggers
+{
+	[ReflectorType("filterTrigger")]
+	public class FilterTrigger : ITrigger
+	{
+		private readonly DateTimeProvider dtProvider;
+		private TimeSpan startTime;
+		private TimeSpan endTime;
+		private DayOfWeek[] weekDays = (DayOfWeek[]) DayOfWeek.GetValues(typeof (DayOfWeek));
+
+		public FilterTrigger() : this(new DateTimeProvider())
+		{
+		}
+
+		public FilterTrigger(DateTimeProvider dtProvider)
+		{
+			this.dtProvider = dtProvider;
+		}
+
+		[ReflectorProperty("trigger", InstanceTypeKey="type")]
+		public ITrigger InnerTrigger;
+
+		[ReflectorProperty("startTime")]
+		public string StartTime
+		{
+			get { return startTime.ToString(); }
+			set { startTime = ParseTime(value); }
+		}
+
+		[ReflectorProperty("endTime")]
+		public string EndTime
+		{
+			get { return endTime.ToString(); }
+			set { endTime = ParseTime(value); }
+		}
+
+		private TimeSpan ParseTime(string timeString)
+		{
+			return TimeSpan.Parse(timeString);
+		}
+
+
+		[ReflectorProperty("buildCondition", Required=false)]
+		public BuildCondition BuildCondition = BuildCondition.NoBuild;
+
+		public BuildCondition ShouldRunIntegration()
+		{
+			DateTime now = dtProvider.Now;
+			if (IsInFilterRange(now))
+			{
+				return BuildCondition;
+			}
+			return InnerTrigger.ShouldRunIntegration();
+		}
+
+		private bool IsInFilterRange(DateTime now)
+		{
+			return IsDateInFilterWeekDays(now) && IsTimeInFilterTimeRange(now);
+		}
+
+		private bool IsDateInFilterWeekDays(DateTime dateTime)
+		{
+			return Array.IndexOf(WeekDays, dateTime.DayOfWeek) >= 0;
+		}
+
+		private bool IsTimeInFilterTimeRange(DateTime dateTime)
+		{
+			TimeSpan timeOfDay = dateTime.TimeOfDay;
+			if (startTime < endTime){
+				return timeOfDay >= startTime && dateTime.TimeOfDay <= endTime;
+			} 
+			else 
+			{
+				return !(timeOfDay <= startTime) || !(dateTime.TimeOfDay >= endTime);
+			}
+		}
+
+		public void IntegrationCompleted()
+		{
+			InnerTrigger.IntegrationCompleted();
+		}
+
+		public DateTime NextBuild
+		{
+			get
+			{
+				DateTime innerTriggerBuild = InnerTrigger.NextBuild;
+				if (IsInFilterRange(innerTriggerBuild))
+				{
+					DateTime nextBuild = new DateTime(innerTriggerBuild.Year, innerTriggerBuild.Month, innerTriggerBuild.Day);
+					nextBuild += endTime;
+					return nextBuild;
+				}
+				return innerTriggerBuild;
+			}
+		}
+
+		[ReflectorArray("weekDays", Required=false)]
+		public DayOfWeek[] WeekDays
+		{
+			get { return weekDays; }
+			set
+			{
+				if (value.Length != 0)
+				{
+					weekDays = value;
+				}
+			}
+		}
+	}
+}
