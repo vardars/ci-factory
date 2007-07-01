28c206e91aff52a53274a32665be4bf7158aa7ac
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-07-01 12:52:41 +0000

    CCNET build 0.8.0.158

---
+++ Product/Production/CCNet/WebDashboard/MVC/NameValueCollectionRequest.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Specialized;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.MVC
+{
+	// ToDo - test!!
+	public class NameValueCollectionRequest : IRequest
+	{
+		private readonly NameValueCollection map;
+
+		public NameValueCollectionRequest(NameValueCollection map)
+		{
+			this.map = map;
+		}
+
+		public string FindParameterStartingWith(string prefix)
+		{
+			foreach (string key in map.Keys)
+			{
+				if (key.StartsWith(prefix))
+				{
+					return key;
+				}
+			}
+			return "";
+		}
+
+		public string GetText(string id)
+		{
+			string text = map[id];
+			if (text == null || text == string.Empty)
+			{
+				return string.Empty;
+			}
+			else
+			{
+				return text;
+			}
+		}
+
+		public bool GetChecked(string id)
+		{
+			string value = GetText(id);
+			return (value != null && value =="on");
+		}
+
+		public int GetInt(string id, int defaultValue)
+		{
+			// To Do - something more sensible
+			string text = GetText(id);
+			if (text != null && text != string.Empty)
+			{
+				try
+				{
+					return int.Parse(text);		
+				}
+				catch (FormatException)
+				{
+					// Todo - exception?
+					return defaultValue;
+				}
+			}
+			else
+			{
+				return defaultValue;
+			}
+		}
+
+		public NameValueCollection Params
+		{
+			get { return map; }
+		}
+	}
+}
