96ef4ac65ae24e0be4ba89b43b67dfd8f0345d90
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-08-26 02:43:16 +0000

    Modified by build script in build 0.9.0.4

--- Current/Product/Production/Nant/CIFactory.NAnt.CCNet.Tasks/Properties/AssemblyInfo.cs
+++ Current/Product/Production/Nant/CIFactory.NAnt.CCNet.Tasks/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CIFactory.NAnt.CCNet.Tasks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
+[assembly: AssemblyCopyright("Copyright �  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
