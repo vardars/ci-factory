201258bf2c30ae54ca15c5faf48b9f59b8cdad57
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-09-03 20:03:41 +0000

    Modified by build script in build 0.9.0.8

--- Current/Product/Production/CCNet/core/Properties/AssemblyInfo.cs
+++ Current/Product/Production/CCNet/core/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 [assembly : AssemblyTitle("CC.NET Core Library")]
 [assembly : AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
