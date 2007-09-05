1163e9930d3ed742b0f91032d12b6b83a115bae0
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-09-05 00:53:22 +0000

    CCNET build 0.9.0.11

---
+++ tags/0.9.0.11/Product/Production/CCNet/WebDashboard/MVC/RedirectResponse.cs
@@ -0,0 +1,17 @@
+namespace ThoughtWorks.CruiseControl.WebDashboard.MVC
+{
+	public class RedirectResponse : IResponse
+	{
+		private readonly string redirectUrl;
+
+		public RedirectResponse(string redirectURL)
+		{
+			redirectUrl = redirectURL;
+		}
+
+		public string Url
+		{
+			get { return redirectUrl; }
+		}
+	}
+}
