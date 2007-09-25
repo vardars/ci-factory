a3854645bea07cba0067710e72ac66df9c7bdc60
jay.flowers <jay.flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-09-25 19:50:10 +0000

    Remove hard coded C:\Program Files

--- Current/Build/OpenSolution.bat
+++ Current/Build/OpenSolution.bat
@@ -1,3 +1,3 @@
 set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;.\nAnt\bin
-call "C:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat"
+call "%ProgramFiles%\Microsoft Visual Studio 8\VC\vcvarsall.bat"
 devenv "CI FactoryBuild.sln"
