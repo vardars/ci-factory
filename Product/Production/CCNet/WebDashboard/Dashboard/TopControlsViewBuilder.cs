4ac9278e186b0289a9523f1e76885c80c858b617
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-09-27 15:55:10 +0000

    CCNET build 0.9.0.31

---
+++ Product/Production/CCNet/WebDashboard/Dashboard/TopControlsViewBuilder.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using ThoughtWorks.CruiseControl.WebDashboard.IO;
+using ThoughtWorks.CruiseControl.WebDashboard.MVC;
+using ThoughtWorks.CruiseControl.WebDashboard.MVC.View;
+using ThoughtWorks.CruiseControl.WebDashboard.Plugins.BuildReport;
+using ThoughtWorks.CruiseControl.WebDashboard.Plugins.FarmReport;
+using ThoughtWorks.CruiseControl.WebDashboard.Plugins.ProjectReport;
+using ThoughtWorks.CruiseControl.WebDashboard.Plugins.ServerReport;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.Dashboard
+{
+	public class TopControlsViewBuilder
+	{
+		private readonly ICruiseRequest request;
+		private readonly ILinkFactory linkFactory;
+		private readonly IVelocityViewGenerator velocityViewGenerator;
+
+		public TopControlsViewBuilder(ICruiseRequest request, ILinkFactory linkFactory, IVelocityViewGenerator velocityViewGenerator)
+		{
+			this.request = request;
+			this.linkFactory = linkFactory;
+			this.velocityViewGenerator = velocityViewGenerator;
+		}
+
+		public HtmlFragmentResponse Execute()
+		{
+			Hashtable velocityContext = new Hashtable();
+
+			string serverName = request.ServerName;
+			string projectName = request.ProjectName;
+			string buildName = request.BuildName;
+
+			velocityContext["serverName"] = serverName;
+			velocityContext["projectName"] = projectName;
+			velocityContext["buildName"] = buildName;
+
+			velocityContext["farmLink"] = linkFactory.CreateFarmLink("Dashboard", FarmReportFarmPlugin.ACTION_NAME);
+
+			if (serverName != "")
+			{
+				velocityContext["serverLink"] = linkFactory.CreateServerLink(request.ServerSpecifier, ServerReportServerPlugin.ACTION_NAME);
+			}
+
+			if (projectName != "")
+			{
+				velocityContext["projectLink"] = linkFactory.CreateProjectLink(request.ProjectSpecifier,  ProjectReportProjectPlugin.ACTION_NAME);
+			}
+
+			if (buildName != "")
+			{
+				velocityContext["buildLink"] = linkFactory.CreateBuildLink(request.BuildSpecifier,  BuildReportBuildPlugin.ACTION_NAME);
+			}
+
+			return velocityViewGenerator.GenerateView("TopMenu.vm", velocityContext);
+		}
+	}
+}
