c94b60f331cfe23009679ed15f39c732d5c5d4b1
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-09-27 00:13:15 +0000

    CCNET build 0.9.0.30

---
+++ tags/0.9.0.30/Product/Production/Nant/CIFactory.NAnt.UnitTest.Tasks/UpdatenAnt.bat
@@ -0,0 +1,15 @@
+:: "$(ProjectDir)UpdatenAnt.bat" "$(SolutionDir)" "$(TargetDir)" $(ProjectName)
+cd %1
+mkdir "NAnt Tests\bin"
+cd "NAnt Tests\bin"
+FOR /F "TOKENS=1 DELIMS=," %%A IN ('cd') DO SET Dest=%%A
+
+cd %2
+copy %3.dll "%Dest%"
+copy %3.pdb "%Dest%"
+
+cd "C:\Projects\CI Factory\Current\Third Party\MbUnit"
+xcopy *.* "%Dest%" /S /Y
+
+cd %1
+nant -buildfile:DevEnv\SetupScripts\SetUp.xml
