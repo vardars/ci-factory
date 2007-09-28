2006fad38ed9999c1576eaf17fac94564b1a5daf
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-09-28 04:33:40 +0000

    CCNET build 0.9.0.35

---
+++ Product/Production/CCNet/WebDashboard/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly : AssemblyTitle("CC.NET Web Dashboard")]
+[assembly : AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
