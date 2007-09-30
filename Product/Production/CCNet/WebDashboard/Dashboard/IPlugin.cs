2bd4832b4dc8020a38f89cb627f5de910f3c3e19
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-09-30 04:22:15 +0000

    CCNET build 0.9.0.48

---
+++ Product/Production/CCNet/WebDashboard/Dashboard/IPlugin.cs
@@ -0,0 +1,9 @@
+namespace ThoughtWorks.CruiseControl.WebDashboard.Dashboard
+{
+	public interface IPlugin
+	{
+		INamedAction[] NamedActions { get; }
+		string LinkDescription { get; }
+        string ImageFileName { get; set; }
+	}
+}
