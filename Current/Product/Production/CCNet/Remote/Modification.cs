90885842c6cf2c712fbdf07b2b6156148211954f
Jay.Flowers <Jay.Flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-09-30 23:21:55 +0000

    Added mods and forcee to project grid

--- Current/Product/Production/CCNet/Remote/Modification.cs
+++ Current/Product/Production/CCNet/Remote/Modification.cs
@@ -7,77 +7,234 @@ using System.Text;
 
 namespace ThoughtWorks.CruiseControl.Remote
 {
-	/// <summary>
-	/// Value object representing the data associated with a source control modification.
-	/// </summary>
+    /// <summary>
+    /// Value object representing the data associated with a source control modification.
+    /// </summary>
     [Serializable]
-	[XmlRoot("modification")]
-	public class Modification : IComparable
-	{
-		public string Type = "unknown";
-		public string FileName;
-		public string FolderName;
-		public DateTime ModifiedTime;
-		public string UserName;
-		public int ChangeNumber;
-		public string Version = "";
-		public string Comment;
-		public string Url;
-		public string EmailAddress;
+    [XmlRoot("modification")]
+    public class Modification : IComparable
+    {
+        #region Fields
 
-		public string ToXml()
-		{
-			StringWriter writer = new StringWriter();
-			ToXml(new XmlTextWriter(writer));
-			return writer.ToString();
-		}
+        private int _ChangeNumber;
 
-		public void ToXml(XmlWriter writer)
-		{
-			writer.WriteStartElement("modification");
-			writer.WriteAttributeString("type", Type);
-			writer.WriteElementString("filename", FileName);
-			writer.WriteElementString("project", FolderName);
-			writer.WriteElementString("date", ModifiedTime.ToString("yyyy-MM-dd HH:mm:ss"));
-			writer.WriteElementString("user", UserName);
-			writer.WriteElementString("comment", Comment);
-			writer.WriteElementString("changeNumber", ChangeNumber.ToString());
+        private string _Comment;
 
-			if (! (Version == null || Version.Length == 0)) writer.WriteElementString("version", Version);
+        private string _EmailAddress;
+
+        private string _FileName;
+
+        private string _FolderName;
+
+        private DateTime _ModifiedTime;
+
+        private string _Type = "unknown";
+
+        private string _Url;
+
+        private string _UserName;
+
+        private string _Version = "";
+
+        #endregion
+
+        #region Properties
+
+        public int ChangeNumber
+        {
+            get
+            {
+                return _ChangeNumber;
+            }
+            set
+            {
+                if (_ChangeNumber == value)
+                    return;
+                _ChangeNumber = value;
+            }
+        }
+
+        public string Comment
+        {
+            get
+            {
+                return _Comment;
+            }
+            set
+            {
+                if (_Comment == value)
+                    return;
+                _Comment = value;
+            }
+        }
+
+        public string EmailAddress
+        {
+            get
+            {
+                return _EmailAddress;
+            }
+            set
+            {
+                _EmailAddress = value;
+            }
+        }
+
+        public string FileName
+        {
+            get
+            {
+                return _FileName;
+            }
+            set
+            {
+                if (_FileName == value)
+                    return;
+                _FileName = value;
+            }
+        }
+
+        public string FolderName
+        {
+            get
+            {
+                return _FolderName;
+            }
+            set
+            {
+                if (_FolderName == value)
+                    return;
+                _FolderName = value;
+            }
+        }
+
+        public DateTime ModifiedTime
+        {
+            get
+            {
+                return _ModifiedTime;
+            }
+            set
+            {
+                if (_ModifiedTime == value)
+                    return;
+                _ModifiedTime = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return _Type;
+            }
+            set
+            {
+                _Type = value;
+            }
+        }
+
+        public string Url
+        {
+            get
+            {
+                return _Url;
+            }
+            set
+            {
+                if (_Url == value)
+                    return;
+                _Url = value;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return _UserName;
+            }
+            set
+            {
+                if (_UserName == value)
+                    return;
+                _UserName = value;
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return _Version;
+            }
+            set
+            {
+                if (_Version == value)
+                    return;
+                _Version = value;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public int CompareTo(Object o)
+        {
+            Modification modification = (Modification)o;
+            return ModifiedTime.CompareTo(modification.ModifiedTime);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ReflectionUtil.ReflectionEquals(this, obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return ReflectionUtil.ReflectionToString(this);
+        }
+
+        public void ToXml(XmlWriter writer)
+        {
+            writer.WriteStartElement("modification");
+            writer.WriteAttributeString("type", Type);
+            writer.WriteElementString("filename", FileName);
+            writer.WriteElementString("project", FolderName);
+            writer.WriteElementString("date", ModifiedTime.ToString("yyyy-MM-dd HH:mm:ss"));
+            writer.WriteElementString("user", UserName);
+            writer.WriteElementString("comment", Comment);
+            writer.WriteElementString("changeNumber", ChangeNumber.ToString());
+
+            if (!(Version == null || Version.Length == 0)) writer.WriteElementString("version", Version);
 
             if (Url != null)
             {
                 writer.WriteElementString("url", Url);
             }
 
-            if (EmailAddress != null)
+            if (_EmailAddress != null)
             {
-                writer.WriteElementString("email", EmailAddress);
+                writer.WriteElementString("email", _EmailAddress);
             }
 
-			writer.WriteEndElement();
-		}
+            writer.WriteEndElement();
+        }
 
-		public int CompareTo(Object o)
-		{
-			Modification modification = (Modification) o;
-			return ModifiedTime.CompareTo(modification.ModifiedTime);
-		}
+        public string ToXml()
+        {
+            StringWriter writer = new StringWriter();
+            ToXml(new XmlTextWriter(writer));
+            return writer.ToString();
+        }
 
-		public override int GetHashCode()
-		{
-			return ToString().GetHashCode();
-		}
-
-		public override bool Equals(object obj)
-		{
-			return ReflectionUtil.ReflectionEquals(this, obj);
-		}
-
-		public override string ToString()
-		{
-			return ReflectionUtil.ReflectionToString(this);
-		}
+        #endregion
 
         private class ReflectionUtil
         {
@@ -192,5 +349,5 @@ namespace ThoughtWorks.CruiseControl.Remote
                 return buffer.ToString();
             }
         }
-	}
+    }
 }
