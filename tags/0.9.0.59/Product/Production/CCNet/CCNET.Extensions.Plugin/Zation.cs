3a2d4fd60b294aa0064b3a5c8fd3e02053b40344
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-10-02 21:40:23 +0000

    CCNET build 0.9.0.59

---
+++ tags/0.9.0.59/Product/Production/CCNet/CCNET.Extensions.Plugin/Zation.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Xml;
+using System.IO;
+using System.Collections;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+using System.ComponentModel;
+
+namespace CCNET.Extensions
+{
+
+    public class Zation
+    {
+
+        public static string Serialize(string reflectorType, object subject)
+        {
+            StringWriter buffer = new StringWriter();
+            new ReflectorTypeAttribute(reflectorType).Write(new XmlTextWriter(buffer), subject);
+            return buffer.ToString();
+        }
+
+        public static object Deserialize(string serialized)
+        {
+            return NetReflector.Read(serialized);
+        }
+    }
+
+}
