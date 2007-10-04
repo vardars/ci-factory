e44e7c858a6826eaeac4234e72bc154c1ab2f845
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-10-04 14:42:27 +0000

    CCNET build 0.9.0.62

---
+++ Product/Production/CCNet/Remote/ProjectStatus.cs
@@ -0,0 +1,116 @@
+using System;
+
+namespace ThoughtWorks.CruiseControl.Remote
+{
+	/// <summary>
+	/// Value type that contains extensive details about a project's most recent
+	/// integration.
+	/// </summary>
+	/// <remarks>
+	/// This class is serialized to persist CruiseControl.NET's state for a
+	/// particular project, hence is is marked with a <see cref="SerializableAttribute"/>.
+	/// </remarks>
+	[Serializable]
+	public class ProjectStatus
+	{
+		private ProjectIntegratorState status;
+		private IntegrationStatus buildStatus;
+		private ProjectActivity activity;
+		private string name;
+		private string webURL;
+		private DateTime lastBuildDate;
+		private string lastBuildLabel;
+		private string lastSuccessfulBuildLabel;
+		private readonly DateTime nextBuildTime;
+        private string _Forcee;
+        private Modification[] _Modifications;
+
+		public ProjectStatus() { }
+
+		public ProjectStatus(ProjectIntegratorState status, IntegrationStatus buildStatus, ProjectActivity activity, string name, string webURL, DateTime lastBuildDate, string lastBuildLabel, string lastSuccessfulBuildLabel, DateTime nextBuildTime, string forcee, Modification[] modifications) 
+		{
+			this.status = status;
+			this.buildStatus = buildStatus;
+			this.activity = activity;
+			this.name = name;
+			this.webURL = webURL;
+			this.lastBuildDate = lastBuildDate;
+			this.lastBuildLabel = lastBuildLabel;
+			this.lastSuccessfulBuildLabel = lastSuccessfulBuildLabel;
+			this.nextBuildTime = nextBuildTime;
+            this._Forcee = forcee;
+            this._Modifications = modifications;
+		}
+
+        public Modification[] Modifications
+        {
+            get { return _Modifications; }
+            set
+            {
+                _Modifications = value;
+            }
+        }
+        
+        public string Forcee
+        {
+            get { return _Forcee; }
+            set
+            {
+                _Forcee = value;
+            }
+        }
+
+		public ProjectIntegratorState Status 
+		{
+			get { return status; }
+			set { status = value; }
+		}
+
+		public IntegrationStatus BuildStatus 
+		{
+			get { return buildStatus; }
+			set { buildStatus = value; }
+		}
+
+		public ProjectActivity Activity 
+		{
+			get { return activity; }
+			set { activity = value; }
+		}
+
+		public string Name 
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public string WebURL
+		{
+			get { return webURL; }
+			set { webURL = value; }
+		}
+
+		public DateTime LastBuildDate
+		{
+			get { return lastBuildDate; }
+			set { lastBuildDate = value; }
+		}
+
+		public string LastBuildLabel
+		{
+			get { return lastBuildLabel; }
+			set { lastBuildLabel = value; }
+		}
+
+		public string LastSuccessfulBuildLabel
+		{
+			get { return lastSuccessfulBuildLabel; }
+			set { lastSuccessfulBuildLabel = value; }
+		}
+
+		public DateTime NextBuildTime
+		{
+			get { return nextBuildTime; }
+		}
+	}
+}
---
+++ Product/Production/CCNet/core/configuration/XmlValidatingLoader.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace ThoughtWorks.CruiseControl.Core.Config
+{
+	/// <summary>
+	/// Summary description for XmlValidatingLoader.
+	/// </summary>
+	public class XmlValidatingLoader
+	{
+        private readonly XmlTextReader innerReader;
+        private XmlReaderSettings xmlReaderSettings;
+        private bool valid;
+
+        public XmlValidatingLoader(XmlTextReader innerReader)
+        {
+            this.innerReader = innerReader;
+            innerReader.EntityHandling = EntityHandling.ExpandEntities;
+            xmlReaderSettings = new XmlReaderSettings();
+            xmlReaderSettings.ValidationType = ValidationType.None;
+            xmlReaderSettings.ProhibitDtd = false;
+            xmlReaderSettings.ValidationEventHandler += new ValidationEventHandler(ValidationHandler);
+        }
+
+        public event ValidationEventHandler ValidationEventHandler
+        {
+            add { xmlReaderSettings.ValidationEventHandler += value; }
+            remove { xmlReaderSettings.ValidationEventHandler -= value; }
+        }
+
+        public void AddSchema(XmlSchema schema)
+        {
+            xmlReaderSettings.Schemas.Add(schema);
+            xmlReaderSettings.ValidationType = ValidationType.Schema;
+        }
+
+        public XmlDocument Load()
+        {
+            // lock in case this object is used in a multi-threaded situation
+            lock (this)
+            {
+                // set the flag true
+                valid = true;
+
+                using (XmlReader reader = XmlReader.Create(innerReader, xmlReaderSettings))
+                {
+                    try
+                    {
+                        XmlDocument doc = new XmlDocument();
+                        doc.Load(reader);
+
+                        // if the load failed, our event handler will have set flag to false
+                        return valid ? doc : null;
+                    }
+                    finally
+                    {
+                        valid = true;
+                    }
+                }
+            }
+        }
+
+        private void ValidationHandler(object sender, ValidationEventArgs args)
+        {
+            valid = false;
+        }
+
+	}
+}
