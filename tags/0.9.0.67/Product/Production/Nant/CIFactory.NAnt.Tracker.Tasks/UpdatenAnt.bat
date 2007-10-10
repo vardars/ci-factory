6387f39f90e878cc8d8fe7ebd5d5b265e5476c14
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-10-10 03:11:24 +0000

    CCNET build 0.9.0.67

---
+++ tags/0.9.0.67/Product/Production/Nant/CIFactory.NAnt.Tracker.Tasks/UpdatenAnt.bat
@@ -0,0 +1,8 @@
+:: "$(ProjectDir)UpdatenAnt.bat" "$(SolutionDir)" "$(TargetDir)" "$(ProjectName)"
+mkdir %1Production\Packages\Tracker\bin
+copy %2%3.dll %1Production\Packages\Tracker\bin
+copy %2%3.pdb %1Production\Packages\Tracker\bin
+copy %2Tracker.Common.dll %1Production\Packages\Tracker\bin
+copy %2Tracker.Common.pdb %1Production\Packages\Tracker\bin
+cd %1
+nant -buildfile:DevEnv\SetupScripts\SetUp.xml
