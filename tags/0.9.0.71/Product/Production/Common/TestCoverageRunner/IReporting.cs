648855110dc6b3b6e5381dd6d1c747dd3ad9500f
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-10-11 04:18:46 +0000

    CCNET build 0.9.0.71

---
+++ tags/0.9.0.71/Product/Production/Common/TestCoverageRunner/IReporting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TestCoverageRunner
+{
+    public interface IReporting
+    {
+        void GenerateReport();
+        System.Collections.Specialized.StringCollection ProductionAssemblies { get; set; }
+        string ProjectName { get; set; }
+        System.IO.Stream ReportStream { get; set; }
+        System.Collections.Specialized.StringCollection TestAssemblies { get; set; }
+    }
+}
