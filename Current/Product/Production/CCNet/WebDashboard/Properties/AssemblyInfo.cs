1e8c9949ae7180af9c35c70cf7566c2932cc6929
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-10-16 15:39:06 +0000

    Modified by build script in build 0.9.0.73

--- Current/Product/Production/CCNet/WebDashboard/Properties/AssemblyInfo.cs
+++ Current/Product/Production/CCNet/WebDashboard/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 [assembly : AssemblyTitle("CC.NET Web Dashboard")]
 [assembly : AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyVersion("1.0.0.18")]
+[assembly: AssemblyVersion("1.0.0.19")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
