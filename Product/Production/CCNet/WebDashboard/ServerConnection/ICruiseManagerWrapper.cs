f85307153fd33f83f70e5eb3ed837198b0d17062
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-10-17 16:12:27 +0000

    CCNET build 0.9.0.76

---
+++ Product/Production/CCNet/WebDashboard/ServerConnection/ICruiseManagerWrapper.cs
@@ -0,0 +1,17 @@
+using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
+using ThoughtWorks.CruiseControl.WebDashboard.Dashboard;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.ServerConnection
+{
+	public interface ICruiseManagerWrapper
+	{
+		IBuildSpecifier GetLatestBuildSpecifier(IProjectSpecifier projectSpecifier);
+		string GetLog(IBuildSpecifier buildSpecifier);
+		IBuildSpecifier[] GetBuildSpecifiers(IProjectSpecifier projectSpecifier);
+		string GetServerLog(IServerSpecifier serverSpecifier);
+		IServerSpecifier[] GetServerSpecifiers();
+		void AddProject(IServerSpecifier serverSpecifier, string serializedProject);
+		string GetProject(IProjectSpecifier projectSpecifier);
+		void UpdateProject(IProjectSpecifier projectSpecifier, string serializedProject);
+	}
+}
