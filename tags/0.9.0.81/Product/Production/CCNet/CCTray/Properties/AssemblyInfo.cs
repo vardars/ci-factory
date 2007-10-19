103b779a7105c51194adb34dd48a53e26a401476
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-10-19 01:00:14 +0000

    CCNET build 0.9.0.81

---
+++ tags/0.9.0.81/Product/Production/CCNet/CCTray/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly : AssemblyTitle("CruiseControl.NET Multi-Project Tray Application")]
+[assembly : AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
