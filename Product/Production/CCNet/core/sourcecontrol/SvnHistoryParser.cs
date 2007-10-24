11f0fd1331dc0db7a4ed763addfb5252d4f12f8f
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-10-24 21:02:49 +0000

    CCNET build 0.9.0.91

---
+++ Product/Production/CCNet/core/sourcecontrol/SvnHistoryParser.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections;
+using System.Globalization;
+using System.IO;
+using System.Xml;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ThoughtWorks.CruiseControl.Core.Sourcecontrol
+{
+	public class SvnHistoryParser : IHistoryParser
+	{
+		public Modification[] Parse(TextReader svnLog, DateTime from, DateTime to)
+		{
+			ArrayList mods = new ArrayList();
+
+			XmlNode svnLogRoot = ReadSvnLogIntoXmlNode(svnLog);
+			XmlNodeList logEntries = svnLogRoot.SelectNodes("/log/logentry");
+			foreach (XmlNode logEntry in logEntries)
+			{
+				mods.AddRange(ParseModificationsFromLogEntry(logEntry, from, to));
+			}
+			return (Modification[]) mods.ToArray(typeof(Modification));
+		}
+
+		private XmlNode ReadSvnLogIntoXmlNode(TextReader svnLog)
+		{
+			string logText = svnLog.ReadToEnd();
+
+			XmlDocument log = new XmlDocument();
+			try
+			{
+				log.LoadXml(logText);
+			}
+			catch (XmlException ex)
+			{
+				throw new CruiseControlException(string.Format("Unable to load the output from svn: {0}", logText), ex);
+			}
+			return log.DocumentElement;
+		}
+
+		private ArrayList ParseModificationsFromLogEntry(XmlNode logEntry, DateTime from, DateTime to)
+		{
+			DateTime changeTime = ParseDate(logEntry);
+			if (changeTime < from || to < changeTime)
+			{
+				// Work around issue 1642 in Subversion (http://subversion.tigris.org/issues/show_bug.cgi?id=1642).
+				return new ArrayList();
+			}
+
+			int changeNumber = ParseChangeNumber(logEntry);
+			string author = ParseAuthor(logEntry);
+			string message = ParseMessage(logEntry);
+
+			ArrayList mods = new ArrayList();
+			XmlNodeList paths = logEntry.SelectNodes("paths/path");
+			foreach (XmlNode path in paths)
+			{
+				Modification mod = new Modification();
+				mod.ChangeNumber = changeNumber;
+				mod.ModifiedTime = changeTime;
+				mod.UserName = author;
+				mod.Comment = message;
+				mod.Type = ModificationType(path);
+				string fullFileName = path.InnerText;
+				mod.FolderName = GetFolderFromPath(fullFileName);
+				mod.FileName = GetFileFromPath(fullFileName);
+				mods.Add(mod);
+			}
+			return mods;
+		}
+
+		private string ModificationType(XmlNode path)
+		{
+			string action = GetAttributeFromNode(path, "action");
+			switch (action)
+			{
+				case "A":
+					return "Added";
+				case "D":
+					return "Deleted";
+				case "M":
+					return "Modified";
+				case "R":
+					return "Replaced";
+				default:
+					return "Unknown action: " + action;
+			}
+		}
+
+		private static string ParseMessage(XmlNode logEntry)
+		{
+			XmlNode msgNode = logEntry.SelectSingleNode("msg");
+			return msgNode.InnerText;
+		}
+
+		private string ParseAuthor(XmlNode logEntry)
+		{
+			String author = "";
+			XmlNode authorNode = logEntry.SelectSingleNode("author");
+			if (authorNode != null)
+			{
+				author = authorNode.InnerText;
+			}
+			return author;
+		}
+
+		private DateTime ParseDate(XmlNode logEntry)
+		{
+			XmlNode dateNode = logEntry.SelectSingleNode("date");
+			return ParseDate(dateNode.InnerText);
+		}
+
+		private int ParseChangeNumber(XmlNode logEntry)
+		{
+			String revision = GetAttributeFromNode(logEntry, "revision");
+			return int.Parse(revision);
+		}
+
+		private string GetFolderFromPath(string fullFileName)
+		{
+			int lastSlashIdx = fullFileName.LastIndexOf("/");
+			return fullFileName.Substring(0, lastSlashIdx);
+		}
+
+		private string GetFileFromPath(string fullFileName)
+		{
+			int lastSlashIdx = fullFileName.LastIndexOf("/");
+			return fullFileName.Substring(lastSlashIdx + 1);
+		}
+
+		private string GetAttributeFromNode(XmlNode node, string attributeName)
+		{
+			XmlAttributeCollection attributes = node.Attributes;
+			XmlAttribute attribute = (XmlAttribute) attributes.GetNamedItem(attributeName);
+			return attribute.InnerText;
+		}
+
+		private DateTime ParseDate(string date)
+		{
+			return DateTime.Parse(date, CultureInfo.InvariantCulture);
+		}
+	}
+}
---
+++ Product/Production/Nant/CIFactory.NAnt.UnitTest.Tasks/Tasks/TestRunnerTask.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CIFactory.NAnt.Types;
+using NAnt.Core;
+using NAnt.Core.Tasks;
+using NAnt.Core.Attributes;
+using NAnt.Core.Types;
+
+using MbUnit;
+using MbUnit.Framework;
+
+namespace CIFactory.NAnt.UnitTest.Tasks
+{
+    
+    [TaskName("testrunner")]
+    public class TestRunnerTask : Task
+    {
+        #region Constants
+
+        private const string FixtureSetUp = "FixtureSetUp";
+
+        private const string FixtureTearDown = "FixtureTearDown";
+
+        private const string SetUpName = "SetUp";
+
+        private const string TearDownName = "TearDown";
+
+        #endregion
+
+        #region Fields
+
+        private StringList _Fixtures;
+
+        #endregion
+
+        #region Properties
+
+        [BuildElement("fixtures", Required = true)]
+        public StringList Fixtures
+        {
+            get
+            {
+                return _Fixtures;
+            }
+            set
+            {
+                _Fixtures = value;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void TryRun(string targetName, SubProject fixtureProject)
+        {
+            if (fixtureProject.Targets.Contains(targetName))
+            {
+                fixtureProject.Targets[targetName].Execute();
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void ExecuteTask()
+        {
+            int TestCount = 0;
+            int TestPassingCount = 0;
+            StringBuilder FailingTestInfo = new StringBuilder();
+
+            foreach (String Fixture in Fixtures)
+            {
+                SubProject FixtureProject = this.Project.SubProjects[Fixture];
+
+                try
+                {
+                    this.TryRun(string.Format("{0}.{1}", FixtureProject.ProjectName, FixtureSetUp), FixtureProject);
+
+                    foreach (Target FixtureTarget in FixtureProject.Targets)
+                    {
+                        if (FixtureTarget.Name.EndsWith("Test"))
+                        {
+                            try
+                            {
+                                TestCount++;
+                                this.TryRun(string.Format("{0}.{1}", FixtureProject.ProjectName, SetUpName), FixtureProject);
+                                FixtureTarget.Execute();
+                                TestPassingCount++;
+                            }
+                            catch (BuildException bx)
+                            {
+                                string FailureInfo = string.Format("{0} at:{1}{2}", bx.Message, Environment.NewLine, bx.Location.ToString());
+                                Log(Level.Error, FailureInfo);
+                                FailingTestInfo.AppendLine(FailureInfo);
+                            }
+                            catch (Exception ex)
+                            {
+                                string FailureInfo = string.Format("{0} in {1}", ex.Message, FixtureTarget.Name);
+                                Log(Level.Error, FailureInfo);
+                                FailingTestInfo.AppendLine(FailureInfo);
+                            }
+                            finally
+                            {
+                                this.TryRun(string.Format("{0}.{1}", FixtureProject.ProjectName, TearDownName), FixtureProject);
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    this.TryRun(string.Format("{0}.{1}", FixtureProject.ProjectName, FixtureTearDown), FixtureProject);
+                }
+            }
+
+            int TestFailingCount = TestCount - TestPassingCount;
+            Log(Level.Info, "Tests Run: {0}", TestCount);
+            Log(Level.Info, "Tests Passing: {0}", TestPassingCount);
+            Log(Level.Info, "Tests Failing: {0}", TestFailingCount);
+            Log(Level.Info, "Test Assertions Executed: {0}", MbUnit.Framework.Assert.AssertCount);
+            Console.WriteLine(FailingTestInfo.ToString());
+            
+            if (TestFailingCount > 0)
+                throw new BuildException(string.Format("{0} tests failed." , TestFailingCount));
+        }
+
+        #endregion
+
+    }
+}
