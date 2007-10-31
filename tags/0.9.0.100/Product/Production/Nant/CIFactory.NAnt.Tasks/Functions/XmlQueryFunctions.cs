9b9c3260f656dfd6b709e2dd668fdc4b5e44d732
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-10-31 20:15:34 +0000

    CCNET build 0.9.0.100

---
+++ tags/0.9.0.100/Product/Production/Nant/CIFactory.NAnt.Tasks/Functions/XmlQueryFunctions.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Xml;
+using NAnt.Core;
+using NAnt.Core.Types;
+using NAnt.Core.Attributes;
+using CIFactory.NAnt.Types;
+
+namespace CIFactory.NAnt.Functions
+{
+    [FunctionSet("xmlquery", "DataTypes")]
+    public class XmlQueryFunctions : FunctionSetBase
+    {
+        #region Constructors
+
+        public XmlQueryFunctions(Project project, PropertyDictionary properties)
+            : base(project, properties)
+        {
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        [Function("count")]
+        public int Count(String refID)
+        {
+            if (!this.Project.DataTypeReferences.Contains(refID))
+                throw new BuildException(String.Format("The refid {0} is not defined.", refID));
+
+            XmlQuery RefXmlQuery = (XmlQuery)this.Project.DataTypeReferences[refID];
+
+            XmlDocument XmlDoc = new XmlDocument();
+            XmlDoc.Load(RefXmlQuery.File);
+
+            XmlNamespaceManager Manager = new XmlNamespaceManager(XmlDoc.NameTable);
+            foreach (XmlNamespace NameSpace in RefXmlQuery.Namespaces)
+            {
+                if (NameSpace.IfDefined && !NameSpace.UnlessDefined)
+                    Manager.AddNamespace(NameSpace.Prefix, NameSpace.Uri);
+            }
+
+            return XmlDoc.SelectNodes(RefXmlQuery.Query, Manager).Count;
+        }
+
+        #endregion
+
+    }
+}
