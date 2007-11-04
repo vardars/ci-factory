22164ea4d4a23c462d88b68630ed8a6524d344d5
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-11-04 00:19:55 +0000

    CCNET build 0.9.0.109

---
+++ Product/Production/CCNet/WebDashboard/Dashboard/ProjectGrid.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Drawing;
+using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
+using ThoughtWorks.CruiseControl.Remote;
+using ThoughtWorks.CruiseControl.WebDashboard.Plugins.ProjectReport;
+using ThoughtWorks.CruiseControl.WebDashboard.ServerConnection;
+using ThoughtWorks.CruiseControl.WebDashboard.Plugins.BuildReport;
+using System.Text.RegularExpressions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.Dashboard
+{
+	public class ProjectGrid : IProjectGrid
+	{
+		private readonly IUrlBuilder urlBuilder;
+		private readonly ILinkFactory linkFactory;
+
+		public ProjectGrid(IUrlBuilder urlBuilder, ILinkFactory linkFactory)
+		{
+			this.urlBuilder = urlBuilder;
+			this.linkFactory = linkFactory;
+		}
+
+        public ProjectGridRow[] GenerateProjectGridRows(IFarmService farmService, ProjectStatusOnServer[] statusList, string forceBuildActionName,
+            ProjectGridSortColumn sortColumn, bool sortIsAscending)
+        {
+            ArrayList rows = new ArrayList();
+            foreach (ProjectStatusOnServer statusOnServer in statusList)
+            {
+                ProjectStatus status = statusOnServer.ProjectStatus;
+                IServerSpecifier serverSpecifier = statusOnServer.ServerSpecifier;
+                string projectName = status.Name;
+                IProjectSpecifier projectSpecifier = new DefaultProjectSpecifier(serverSpecifier, projectName);
+
+				string baseUrl = Regex.Replace(statusOnServer.ProjectStatus.WebURL, @"default\.aspx\?.*", "");
+
+                string projectLink = baseUrl + linkFactory.CreateProjectLink(projectSpecifier, ProjectReportProjectPlugin.ACTION_NAME).Url;
+                
+                IBuildSpecifier[] buildSpecifiers = farmService.GetMostRecentBuildSpecifiers(projectSpecifier, 1);
+                string mostRecentBuildUrl;
+                if (buildSpecifiers.Length == 1)
+                {
+					mostRecentBuildUrl = baseUrl + linkFactory.CreateProjectLink(projectSpecifier, LatestBuildReportProjectPlugin.ACTION_NAME).Url;
+                }
+                else
+                {
+                    mostRecentBuildUrl = projectLink;
+                }
+
+                rows.Add(
+                    new ProjectGridRow(
+                        projectName, statusOnServer.ServerSpecifier.ServerName, status.BuildStatus.ToString(),
+                        CalculateHtmlColor(status.BuildStatus),
+                        status.LastBuildDate,
+                        (status.LastBuildLabel != null ? status.LastBuildLabel : "no build available"),
+                        status.Status.ToString(),
+                        status.Activity.ToString(),
+                        urlBuilder.BuildFormName(forceBuildActionName),
+                        projectLink,
+                        mostRecentBuildUrl, 
+                        ChangeSet.Convert(status.Modifications), 
+                        status.Forcee
+                    ));
+            }
+
+            rows.Sort(GetComparer(sortColumn, sortIsAscending));
+
+            return (ProjectGridRow[])rows.ToArray(typeof(ProjectGridRow));
+        }
+
+		private string CalculateHtmlColor(IntegrationStatus status)
+		{
+			if (status == IntegrationStatus.Success)
+			{
+				return Color.Green.Name;
+			}
+			else if (status == IntegrationStatus.Unknown)
+			{
+				return Color.Yellow.Name;
+			}
+			else
+			{
+				return Color.Red.Name;
+			}
+		}
+
+		private IComparer GetComparer(ProjectGridSortColumn column, bool ascending)
+		{
+			return new ProjectGridRowComparer(column, ascending);
+		}
+
+		private class ProjectGridRowComparer : IComparer
+		{
+			private readonly ProjectGridSortColumn column;
+			private readonly bool ascending;
+
+			public ProjectGridRowComparer(ProjectGridSortColumn column, bool ascending)
+			{
+				this.column = column;
+				this.ascending = ascending;
+			}
+
+			public int Compare(object x, object y)
+			{
+				ProjectGridRow rowx = x as ProjectGridRow;
+				ProjectGridRow rowy = y as ProjectGridRow;
+				if (column == ProjectGridSortColumn.Name)
+				{
+					return rowx.Name.CompareTo(rowy.Name) * (ascending ? 1 : -1);
+				}
+				else if (column == ProjectGridSortColumn.LastBuildDate)
+				{
+					return rowx.LastBuildDate.CompareTo(rowy.LastBuildDate) * (ascending ? 1 : -1);
+				}
+				else if (column == ProjectGridSortColumn.BuildStatus)
+				{
+					return rowx.BuildStatus.CompareTo(rowy.BuildStatus) * (ascending ? 1 : -1);
+				}
+				else
+				{
+					return 0;
+				}
+			}
+		}
+	}
+}
---
+++ Product/Production/CCNet/core/util/StringUtil.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ThoughtWorks.CruiseControl.Core.Util
+{
+	public class StringUtil
+	{
+		private static Regex NullStringRegex = new Regex("\0");
+
+		public static bool Contains(string text, string fragment)
+		{
+			return text.IndexOf(fragment) > -1;
+		}
+
+		public static bool EqualsIngnoreCase(string a, string b)
+		{
+			return CaseInsensitiveComparer.Default.Compare(a, b) == 0;
+		}
+
+		public static string JoinUnique(string delimiter, params string[][] fragmentArrays)
+		{
+			SortedList list = new SortedList();
+			foreach (string[] fragmentArray in fragmentArrays)
+			{
+				foreach (string fragment in fragmentArray)
+				{
+					if (! list.Contains(fragment))
+						list.Add(fragment, fragment);
+				}
+			}
+			StringBuilder buffer = new StringBuilder();
+			foreach (string value in list.Values)
+			{
+				if (buffer.Length > 0)
+				{
+					buffer.Append(delimiter);
+				}
+				buffer.Append(value);
+			}
+			return buffer.ToString();
+		}
+
+		public static int GenerateHashCode(params string[] values)
+		{
+			int hashcode = 0;
+			foreach (string value in values)
+			{
+				if (value != null)
+				{
+					hashcode += value.GetHashCode();
+				}
+			}
+			return hashcode;
+		}
+
+		public static string LastWord(string input)
+		{
+			return LastWord(input, " .,;!?:");
+		}
+
+		public static string LastWord(string input, string separators)
+		{
+			if (input == null)
+			{
+				return null;
+			}
+			string[] tokens = input.Split(separators.ToCharArray());
+			for (int i = tokens.Length - 1; i >= 0; i--)
+			{
+				if (IsWhitespace(tokens[i]) == false)
+				{
+					return tokens[i].Trim();
+				}
+			}
+			return String.Empty;
+		}
+
+		public static bool IsBlank(string input)
+		{
+			return (input == null || input.Length == 0);
+		}
+
+		public static bool IsWhitespace(string input)
+		{
+			return (input == null || input.Trim().Length == 0);
+		}
+
+		public static string Strip(string input, params string[] removals)
+		{
+			string revised = input;
+			foreach (string removal in removals)
+			{
+				int i = 0;
+				while ((i = revised.IndexOf(removal)) > -1)
+				{
+					revised = revised.Remove(i, removal.Length);
+				}
+			}
+			return revised;
+		}
+
+		public static string[] Insert(string[] input, string insert, int index)
+		{
+			ArrayList list = new ArrayList(input);
+			list.Insert(index, insert);
+			return (string[]) list.ToArray(typeof (string));
+		}
+
+		public static string Join(string separator, params string[] strings)
+		{
+			StringBuilder builder = new StringBuilder();
+			foreach (string s in strings)
+			{
+				if (IsBlank(s)) continue;
+				if (builder.Length > 0) builder.Append(separator);
+				builder.Append(s.ToString());
+			}
+			return builder.ToString();
+		}
+
+		public static string RemoveNulls(string s)
+		{
+			return NullStringRegex.Replace(s, string.Empty).TrimStart();
+		}
+
+		public static string StripQuotes(string filename)
+		{
+			return filename == null ? null : filename.Trim('"');
+		}
+	}
+}
