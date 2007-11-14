7455df7b65b1b21f422abaf019d51281990a17d9
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-11-14 23:24:40 +0000

    CCNET build 0.9.0.122

---
+++ Build/dashboard/xsl/Search.xsl
@@ -0,0 +1,17 @@
+<?xml version="1.0"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+  <xsl:output method="html"/>
+
+  <xsl:template match="/">
+    <div style="overflow: auto; position:absolute; left: 600px; top: 425px;">
+      <!-- Google CSE Search Box Begins  -->
+      <form action="http://www.google.com/cse" id="searchbox_000613400651952722275:nwkbfvcczwm">
+        <input type="hidden" name="cx" value="000613400651952722275:nwkbfvcczwm" />
+        <input type="text" name="q" size="25" />
+        <input type="submit" name="sa" value="Search" />
+      </form>
+      <script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=searchbox_000613400651952722275%3Anwkbfvcczwm"></script>
+      <!-- Google CSE Search Box Ends -->
+    </div>
+  </xsl:template>
+</xsl:stylesheet>
---
+++ Product/Production/CCNet/core/Reporting/Dashboard/Navigation/DefaultUrlBuilder.cs
@@ -0,0 +1,45 @@
+using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
+
+namespace ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation
+{
+	public class DefaultUrlBuilder : IUrlBuilder
+	{
+		public static readonly string CONTROLLER_RELATIVE_URL = "default.aspx";
+		public static readonly string ACTION_PARAMETER_PREFIX = "_action_";
+
+		public string BuildUrl(string action)
+		{
+			return BuildUrl(action, "");
+		}
+
+		public string BuildUrl(string action, string partialQueryString)
+		{
+			return BuildUrl(action, partialQueryString, CONTROLLER_RELATIVE_URL);
+		}
+
+		/// <summary>
+		/// Assumes that the partialQueryString and action have been safely url encoded.
+		/// Instead use a parameter collection and url builder can take care of encoding.
+		/// </summary>
+		public string BuildUrl(string action, string partialQueryString, string baseUrl)
+		{
+			string queryString = string.Format("?{0}{1}=true", ACTION_PARAMETER_PREFIX, action);
+			
+			if (partialQueryString != null && partialQueryString != string.Empty)
+			{
+				if (queryString.Length > 1)
+				{
+					queryString += "&";	
+				}
+				queryString += partialQueryString;
+			}
+
+			return baseUrl + queryString;
+		}
+
+		public string BuildFormName(string action)
+		{
+			return ACTION_PARAMETER_PREFIX + action;
+		}
+	}
+}
