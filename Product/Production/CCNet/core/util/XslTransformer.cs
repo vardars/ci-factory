d422e91663a0b32b1b056cb67f593b75f72dd5bd
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-11-15 02:16:03 +0000

    CCNET build 0.9.0.123

---
+++ Product/Production/CCNet/core/util/XslTransformer.cs
@@ -0,0 +1,68 @@
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using System.Collections.Generic;
+
+namespace ThoughtWorks.CruiseControl.Core.Util
+{
+	public class XslTransformer : ITransformer
+	{
+        public string Transform(string input, string xslFilename, Dictionary<string, string> xslParams)
+		{
+            XslCompiledTransform transform = NewXslTransform(xslFilename);
+
+            using (StringReader inputReader = new StringReader(input))
+            {
+                try
+                {
+                    StringWriter output = new StringWriter();
+                    transform.Transform(new XPathDocument(inputReader), CreateXsltArgs(xslParams), output);
+                    return output.ToString();
+                }
+                catch (XmlException ex)
+                {
+                    throw new CruiseControlException("Unable to execute transform: " + xslFilename, ex);
+                }
+            }
+        }
+
+        private static XslCompiledTransform NewXslTransform(string transformerFileName)
+        {
+            XslCompiledTransform transform = new XslCompiledTransform();
+            LoadStylesheet(transform, transformerFileName);
+            return transform;
+        }
+
+        private static XsltArgumentList CreateXsltArgs(Dictionary<string, string> xsltArgs)
+        {
+            XsltArgumentList args = new XsltArgumentList();
+            if (xsltArgs != null)
+            {
+                foreach (string key in xsltArgs.Keys)
+                {
+                    args.AddParam(key.ToString(), "", xsltArgs[key]);
+                }
+            }
+            return args;
+        }
+
+        private static void LoadStylesheet(XslCompiledTransform transform, string xslFileName)
+        {
+            XsltSettings settings = new XsltSettings(false, true);
+
+            try
+            {
+                transform.Load(xslFileName, settings, new XmlUrlResolver());
+            }
+            catch (FileNotFoundException)
+            {
+                throw new CruiseControlException(string.Format("XSL stylesheet file not found: {0}", xslFileName));
+            }
+            catch (XsltException ex)
+            {
+                throw new CruiseControlException("Unable to load transform: " + xslFileName, ex);
+            }
+        }
+    }
+}
