5b4abddff5b1b9d904f65e5ec539b57eecd4007a
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-11-17 18:45:03 +0000

    CCNET build 0.9.0.134

---
+++ tags/0.9.0.134/Product/Production/CCNet/WebDashboard/Dashboard/PathMappingMultiTransformer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
+using ThoughtWorks.CruiseControl.Core.Util;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.Dashboard
+{
+	public class PathMappingMultiTransformer : IMultiTransformer
+	{
+		private readonly IMultiTransformer transformer;
+		private readonly IPathMapper pathMapper;
+
+		public PathMappingMultiTransformer(IPathMapper pathMapper, IMultiTransformer transformer)
+		{
+			this.pathMapper = pathMapper;
+			this.transformer = transformer;
+		}
+
+        public string Transform(string input, string[] transformerFileNames, Dictionary<string, string> xslParams)
+		{
+			ArrayList mappedFiles = new ArrayList();
+			foreach (string transformerFileName in transformerFileNames)
+			{
+				mappedFiles.Add(pathMapper.GetLocalPathFromURLPath(transformerFileName));
+			}
+            return transformer.Transform(input, (string[])mappedFiles.ToArray(typeof(string)), xslParams);
+		}
+	}
+}
