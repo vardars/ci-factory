a38427a2de9c6f504548b10b6bb8cd8742ace2f8
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-11-22 21:13:00 +0000

    CCNET build 0.9.0.158

---
+++ Product/Unit Test/NAnt/Test.TF.Tasks/TfsCheckinTaskTests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MbUnit.Framework;
+
+namespace Test.TF.Tasks
+{
+    [TestFixture]
+    public class TfsCheckinTaskTests
+    {
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
