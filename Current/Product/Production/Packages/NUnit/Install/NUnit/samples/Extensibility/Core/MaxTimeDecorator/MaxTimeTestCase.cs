b742d4b050708eee1bf95c174694cef9daccbccf
Jay.Flowers <Jay.Flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-12-02 02:58:30 +0000

    Upgrade NDepend to 2.5.0.1382 Upgrade Simian to 2.2.21 Upgrade NUnit to 2.4.5

--- Current/Product/Production/Packages/NUnit/Install/NUnit/samples/Extensibility/Core/MaxTimeDecorator/MaxTimeTestCase.cs
+++ Current/Product/Production/Packages/NUnit/Install/NUnit/samples/Extensibility/Core/MaxTimeDecorator/MaxTimeTestCase.cs
@@ -10,34 +10,67 @@ using System.Reflection;
 namespace NUnit.Core.Extensions
 {
 	/// <summary>
-	/// Summary description for MaxTimeTestCase.D:\Dev\NUnit\nunit-2.4\samples\Extensibility\Core\SampleFixtureExtension\Tests\SampleFixtureExtensionTests.cs
+	/// MaxTimeTestCase is a special form of test case that measures
+	/// the elapsed time to run a test, failing the test if it
+	/// exceeds a certain amount.
+	/// 
+	/// Implementation Notes
+	/// 
+	/// In implementing a Decorator, you need to honor any propeties
+	/// taht were set on the original test before you decorated it
+	/// and also be ready to deal correctly with any other decorators
+	/// that are applied before or after yours.
+	/// 
+	/// There are three basic approaches to doing this:
+	/// 
+	/// 1) Modify or set properties of the original test case 
+	///    and return it. This avoids all issues of ordering
+	///    but won't work if new properties are needed.
+	/// 2) Replace the test case with a new one implementing
+	///    additional properties. Copy all properties from
+	///    the original case. This is the approach used in
+	///    this example.
+	/// 3) Wrap the test case with a special test case that
+	///    delegates all properties and methods to the
+	///    origial test case. This is quite difficult to
+	///    get right, but is necessary in some cases.
 	/// </summary>
-	public class MaxTimeTestCase : TestCase
+	public class MaxTimeTestCase : NUnitTestMethod
 	{
-		private TestCase testCase;
 		private int maxTime = 0;
+		bool expectFailure;
 
-		public MaxTimeTestCase( TestCase testCase, int maxTime )
-			: base( (TestName)testCase.TestName.Clone() )
+		public MaxTimeTestCase( NUnitTestMethod testCase, int maxTime, bool expectFailure )
+			: base( testCase.Method )
 		{
-			// We give it a different test id to avoid confusion
-			// when debugging - even though it's not strictly
-			// necessary in this case.
-			this.TestName.TestID = new TestID();
-			this.testCase = testCase;
 			this.maxTime = maxTime;
+			this.expectFailure = expectFailure;
+
+			// Copy all the attributes of the original test
+			this.Description = testCase.Description;
+			this.Fixture = testCase.Fixture;
+			this.Parent = testCase.Parent;
+			this.RunState = testCase.RunState;
+			this.IgnoreReason = testCase.IgnoreReason;
+			this.ExceptionExpected = testCase.ExceptionExpected;
+			this.ExpectedExceptionName = testCase.ExpectedExceptionName;
+			this.ExpectedExceptionType = testCase.ExpectedExceptionType;
+			this.ExpectedMessage = testCase.ExpectedMessage;
+			this.Properties = testCase.Properties;
+			this.Categories = testCase.Categories;
 		}
 
 		public override void Run(TestCaseResult result)
 		{
-			testCase.Run( result );
-			if ( result.IsSuccess )
+			base.Run( result );
+			if ( result.IsSuccess && !ExceptionExpected )
 			{
 				int elapsedTime = (int)(result.Time * 1000);
-				if ( elapsedTime > maxTime )
+				if ( elapsedTime > maxTime && !expectFailure)
 					result.Failure( string.Format( "Elapsed time of {0}ms exceeds maximum of {1}ms", elapsedTime, maxTime ), null );
+				else if ( elapsedTime <= maxTime && expectFailure )
+					result.Failure( "Expected a timeout failure, but none occured", null );
 			}
 		}
-
 	}
 }
