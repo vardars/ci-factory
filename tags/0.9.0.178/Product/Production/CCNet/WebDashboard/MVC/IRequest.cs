3dcdc65dfa4d332408184fed1b2f2e7f42f717e5
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-12-02 20:22:20 +0000

    CCNET build 0.9.0.178

---
+++ tags/0.9.0.178/Product/Production/CCNet/WebDashboard/MVC/IRequest.cs
@@ -0,0 +1,14 @@
+using System.Collections.Specialized;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.MVC
+{
+	public interface IRequest
+	{
+		string FindParameterStartingWith(string prefix);
+		string GetText(string id);
+		bool GetChecked(string id);
+		int GetInt(string id, int defaultValue);
+        NameValueCollection Params { get; }
+        string ApplicationPath { get; }
+	}
+}
