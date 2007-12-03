4307cc568e09fae3430829c4be025378f6ec9bd3
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-12-03 00:38:57 +0000

    CCNET build 0.9.0.181

---
+++ Product/Unit Test/NAnt/Test.TF.Tasks/TfsAddTaskTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MbUnit.Framework;
+
+using TF.Tasks.SourceControl;
+
+namespace Test.TF.Tasks
+{
+    [TestFixture]
+    public class TfsAddTaskTests
+    {
+        [Test]
+        public void TestAddNewFile()
+        {
+
+        }
+    }
+
+}
