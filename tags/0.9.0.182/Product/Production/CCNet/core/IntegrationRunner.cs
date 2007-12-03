6129383d168516ff48478a9ea3b93255c1d945dd
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-12-03 04:20:04 +0000

    CCNET build 0.9.0.182

---
+++ tags/0.9.0.182/Product/Production/CCNet/core/IntegrationRunner.cs
@@ -0,0 +1,121 @@
+using System;
+using System.IO;
+using ThoughtWorks.CruiseControl.Core.Sourcecontrol;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ThoughtWorks.CruiseControl.Core
+{
+	public class IntegrationRunner : IIntegratable
+	{
+		public IIntegrationRunnerTarget target;
+		private readonly IIntegrationResultManager resultManager;
+		private readonly IQuietPeriod quietPeriod;
+		private readonly IProject _Project;
+
+		public IIntegrationFilter IntegrationFilter
+		{
+			get
+			{
+				return _Project.IntegrationFilter;
+			}
+		}
+
+		public IntegrationRunner(IIntegrationResultManager resultManager, IIntegrationRunnerTarget target, IQuietPeriod quietPeriod, IProject project)
+		{
+			this.target = target;
+			this.quietPeriod = quietPeriod;
+			this.resultManager = resultManager;
+			this._Project = project;
+		}
+
+		public IIntegrationResult RunIntegration(IIntegrationResult result)
+		{
+			IIntegrationResult lastResult = resultManager.LastIntegrationResult;
+
+			this.CreateDirectoryIfItDoesntExist(result.WorkingDirectory);
+			this.CreateDirectoryIfItDoesntExist(result.ArtifactDirectory);
+			result.MarkStartTime();
+			bool IsRunable = false;
+			try
+			{
+				result.Modifications = this.GetModifications(lastResult, result);
+				IsRunable = this.IntegrationFilter.ShouldRunBuild(result);
+				if (IsRunable)
+				{
+					target.Activity = ProjectActivity.Building;
+					target.SourceControl.GetSource(result);
+					this.RunBuild(result);
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Error(ex);
+				result.ExceptionResult = ex;
+			}
+			result.MarkEndTime();
+
+			if (IsRunable)
+				this.PostBuild(result);
+
+			target.Activity = ProjectActivity.Sleeping;
+
+			return result;
+		}
+
+		private Modification[] GetModifications(IIntegrationResult from, IIntegrationResult to)
+		{
+			target.Activity = ProjectActivity.CheckingModifications;
+			return quietPeriod.GetModifications(target.SourceControl, from, to);
+		}
+
+		private void CreateDirectoryIfItDoesntExist(string directory)
+		{
+			if (! Directory.Exists(directory))
+				Directory.CreateDirectory(directory);
+		}
+
+		private void RunBuild(IIntegrationResult result)
+		{
+			Log.Info("Building");
+			target.Run(result);
+			Log.Info("Build complete: " + result.Status);
+		}
+
+		private void PostBuild(IIntegrationResult result)
+		{
+			if (this.ShouldPublishResult(result))
+			{
+				this.LabelSourceControl(result);
+				target.PublishResults(result);
+				resultManager.FinishIntegration();
+			}
+			Log.Info("Integration complete: " + result.EndTime);
+		}
+
+		private void LabelSourceControl(IIntegrationResult result)
+		{
+			try
+			{
+				target.SourceControl.LabelSourceControl(result);
+			}
+			catch (Exception e)
+			{
+				Log.Error(new CruiseControlException("Exception occurred while labelling source control provider.", e));
+			}
+		}
+
+		private bool ShouldPublishResult(IIntegrationResult result)
+		{
+			IntegrationStatus integrationStatus = result.Status;
+			if (integrationStatus == IntegrationStatus.Exception)
+			{
+				return target.PublishExceptions;
+			}
+			else
+			{
+				return integrationStatus != IntegrationStatus.Unknown;
+			}
+		}
+	}
+}
