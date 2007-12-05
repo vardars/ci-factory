c8fba096bf328369a9bdc30db313a9f87a9012d4
Jay.Flowers <Jay.Flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-12-05 19:11:08 +0000

    Upgrade to 0.9.0.195 Change LabelPrefix to 1.0.0.

--- Current/Build/OpenSolution.bat
+++ Current/Build/OpenSolution.bat
@@ -1,3 +1,4 @@
-set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;.\nAnt\bin
+FOR /F "delims=" %%v IN ('cd') DO set BuildDirectory=%%v
+set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%ProgramFiles%\Subversion\bin;%BuildDirectory%\nAnt\bin
 call "%ProgramFiles%\Microsoft Visual Studio 8\VC\vcvarsall.bat"
-devenv "CI FactoryBuild.sln"
+devenv "CI FactoryBuildScripts.sln"
