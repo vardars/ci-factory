8ad4fa5d91bfc1c5e74d4ba543aa597d4fe49a2b
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-12-12 14:36:52 +0000

    CCNET build 1.0.0.10

---
+++ Product/Production/CCNet/CCNET.Extensions.Plugin/ForceFilters/HostNameHelper.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Specialized;
+using System.DirectoryServices;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+using CCNET.Extensions.ForceFilters.UserFilter;
+using System.Web;
+using CCNET.Extensions.Plugin.ForceFilters.PasswordFilter;
+using System.Windows.Forms;
+
+namespace CCNET.Extensions.Plugin.ForceFilters
+{
+    public class HostNameHelper : IHostNameHelper
+    {
+        public string GetHostName()
+        {
+            if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Request.UserHostName;
+            }
+            else
+                return Environment.MachineName;
+        }
+    }
+}
