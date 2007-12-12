83a1add52d87a08c4c019adc9796fe95b3c41273
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-12-12 04:31:36 +0000

    CCNET build 1.0.0.8

---
+++ Product/Production/CCNet/core/IIntegrationResult.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ThoughtWorks.CruiseControl.Core
+{
+	public interface IIntegrationResult
+	{
+		string ProjectName { get; }
+		BuildCondition BuildCondition { get; set; }
+		string WorkingDirectory { get; set; }
+		string Label { get; set; }
+		string LastSuccessfulIntegrationLabel { get; }
+		IntegrationStatus Status { get; set; }
+		IntegrationStatus LastIntegrationStatus { get; }
+		DateTime StartTime { get; set; }
+		DateTime EndTime { get; }
+		TimeSpan TotalIntegrationTime { get; }
+		IList TaskResults { get; }
+		DateTime LastModificationDate { get; }
+		int LastChangeNumber { get; }
+		Modification[] Modifications { get; set; }
+		Exception ExceptionResult { get; set; }
+		string ArtifactDirectory { get; set;}
+		string ProjectUrl { get; set;}
+		string TaskOutput { get; }
+
+		void AddTaskResult(string result);
+		void AddTaskResult(ITaskResult result);
+		void AddIntegrationProperty(string key, string value);
+		void RemoveIntegrationProperty(string key);
+		bool IsInitial();
+		bool HasModifications();
+		bool Failed { get; }
+		bool Fixed { get; }
+		bool Succeeded { get; }
+		void MarkStartTime();
+		void MarkEndTime();
+		string BaseFromArtifactsDirectory(string pathToBase);
+		string BaseFromWorkingDirectory(string pathToBase);
+		IDictionary IntegrationProperties { get; }
+	}
+}
