a6fb6e314648668fb3428a68469fa58cb2d00f8a
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-12-22 15:52:08 +0000

    CCNET build 1.0.0.25

---
+++ tags/1.0.0.25/Product/Production/CCNet/WebDashboard/Dashboard/VelocityProjectGridAction.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections;
+using ThoughtWorks.CruiseControl.Core;
+using ThoughtWorks.CruiseControl.Core.Reporting.Dashboard.Navigation;
+using ThoughtWorks.CruiseControl.WebDashboard.MVC;
+using ThoughtWorks.CruiseControl.WebDashboard.MVC.View;
+using ThoughtWorks.CruiseControl.WebDashboard.ServerConnection;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.Dashboard
+{
+	// ToDo - Test!
+	public class VelocityProjectGridAction : IProjectGridAction
+	{
+		private readonly IFarmService farmService;
+		private readonly IUrlBuilder urlBuilder;
+		private readonly ICruiseUrlBuilder cruiseUrlBuilder;
+		private readonly IVelocityViewGenerator viewGenerator;
+		private readonly IProjectGrid projectGrid;
+
+		public VelocityProjectGridAction(IFarmService farmService, IUrlBuilder urlBuilder, ICruiseUrlBuilder cruiseUrlBuilder, IVelocityViewGenerator viewGenerator, IProjectGrid projectGrid)
+		{
+			this.farmService = farmService;
+			this.urlBuilder = urlBuilder;
+			this.cruiseUrlBuilder = cruiseUrlBuilder;
+			this.viewGenerator = viewGenerator;
+			this.projectGrid = projectGrid;
+		}
+
+		public IResponse Execute(string actionName, IRequest request)
+		{
+			Hashtable velocityContext = new Hashtable();
+			velocityContext["forceBuildMessage"] = ForceBuildIfNecessary(request);
+			return GenerateView(farmService.GetProjectStatusListAndCaptureExceptions(), velocityContext, actionName, request, null);
+		}
+
+		public IResponse Execute(string actionName, IServerSpecifier serverSpecifier, IRequest request)
+		{
+			Hashtable velocityContext = new Hashtable();
+			velocityContext["forceBuildMessage"] = ForceBuildIfNecessary(request);
+			return GenerateView(farmService.GetProjectStatusListAndCaptureExceptions(serverSpecifier), velocityContext, actionName, request, serverSpecifier);
+		}
+
+		private HtmlFragmentResponse GenerateView(ProjectStatusListAndExceptions projectStatusListAndExceptions, Hashtable velocityContext, string actionName, IRequest request, IServerSpecifier serverSpecifier)
+		{
+			ProjectGridSortColumn sortColumn = GetSortColumn(request);
+			bool sortReverse = SortAscending(request);
+
+			velocityContext["projectNameSortLink"] = GenerateSortLink(serverSpecifier, actionName, ProjectGridSortColumn.Name, sortColumn, sortReverse);
+			velocityContext["buildStatusSortLink"] = GenerateSortLink(serverSpecifier, actionName, ProjectGridSortColumn.BuildStatus, sortColumn, sortReverse);
+			velocityContext["lastBuildDateSortLink"] = GenerateSortLink(serverSpecifier, actionName, ProjectGridSortColumn.LastBuildDate, sortColumn, sortReverse);
+			velocityContext["projectGrid"] = projectGrid.GenerateProjectGridRows(
+                farmService, projectStatusListAndExceptions.StatusAndServerList, actionName, sortColumn, sortReverse);
+            velocityContext["serverTimeNow"] = DateTime.Now;
+			velocityContext["exceptions"] = projectStatusListAndExceptions.Exceptions;
+			velocityContext["refreshButtonName"] = urlBuilder.BuildFormName(actionName);
+
+			return viewGenerator.GenerateView(@"ProjectGrid.vm", velocityContext);
+		}
+
+		private bool SortAscending(IRequest request)
+		{
+			return request.FindParameterStartingWith("ReverseSort") == string.Empty;
+		}
+
+		private ProjectGridSortColumn GetSortColumn(IRequest request)
+		{
+			string columnName = request.GetText("SortColumn");
+			if (columnName == string.Empty)
+			{
+				columnName = "Name";
+			}
+			try
+			{
+				return (ProjectGridSortColumn) Enum.Parse(typeof(ProjectGridSortColumn), columnName);	
+			}
+			catch (Exception)
+			{
+				throw new CruiseControlException(string.Format("Error attempting to calculate column to sort. Specified column name was [{0}]", columnName));
+			}
+		}
+
+		private object GenerateSortLink(IServerSpecifier serverSpecifier, string action, ProjectGridSortColumn column, ProjectGridSortColumn currentColumn, bool currentReverse)
+		{
+			string queryString = "SortColumn=" + column.ToString();
+			if (column == currentColumn && !currentReverse)
+			{
+				queryString += "&ReverseSort=ReverseSort";
+			}
+			if (serverSpecifier == null)
+			{
+				return urlBuilder.BuildUrl(action, queryString);
+			}
+			else
+			{
+				return cruiseUrlBuilder.BuildServerUrl(action, serverSpecifier, queryString);
+			}
+		}
+
+		private string ForceBuildIfNecessary(IRequest request)
+		{
+			if (request.FindParameterStartingWith("forcebuild") != string.Empty)
+			{
+				string forceBuildProject = request.GetText("forceBuildProject");
+				bool forceWasAllowed = false;
+				forceWasAllowed = farmService.ForceBuild(
+					new DefaultProjectSpecifier(
+						new DefaultServerSpecifier(request.GetText("forceBuildServer")),
+						forceBuildProject));
+				if (forceWasAllowed)
+				{
+					return string.Format("Build successfully forced for {0}", forceBuildProject);
+				}
+				else
+				{
+					return string.Format("Build was not allowed to be forced for {0}", forceBuildProject);
+				}
+			}
+			else
+			{
+				return "";
+			}
+		}
+	}
+}
