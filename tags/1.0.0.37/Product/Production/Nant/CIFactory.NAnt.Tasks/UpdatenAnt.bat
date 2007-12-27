0f12942fa8c019214e821f54a8342243bba20ac1
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-12-27 14:36:08 +0000

    CCNET build 1.0.0.37

---
+++ tags/1.0.0.37/Product/Production/Nant/CIFactory.NAnt.Tasks/UpdatenAnt.bat
@@ -0,0 +1,15 @@
+:: "$(ProjectDir)UpdatenAnt.bat" "$(SolutionDir)" "$(TargetDir)" $(ProjectName)
+cd %1
+cd ..\Third Party\nAnt\bin
+
+FOR /F "TOKENS=1 DELIMS=," %%A IN ('cd') DO SET Dest=%%A
+
+cd %2
+copy %3.dll "%Dest%"
+copy %3.pdb "%Dest%"
+copy Zeta.WebSpider.dll "%Dest%"
+copy SgmlReaderDll.dll "%Dest%"
+copy NDepend.Helpers.FileDirectoryPath.dll "%Dest%"
+
+cd %1
+nant -buildfile:DevEnv\SetupScripts\SetUp.xml
---
+++ tags/1.0.0.37/Product/Production/Nant/CIFactory.NAnt.UnitTest.Tasks/Functions/AssertionFunctions.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIFactory.NAnt.UnitTest.Functions
+{
+    public class AssertionFunctions
+    {
+    }
+}
