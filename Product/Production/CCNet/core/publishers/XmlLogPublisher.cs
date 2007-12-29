8a0e7c8aa04c6ca9ea9f003b134ff58fb3ac3cba
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2007-12-29 15:57:48 +0000

    CCNET build 1.0.0.42

---
+++ Product/Production/CCNet/core/publishers/XmlLogPublisher.cs
@@ -0,0 +1,67 @@
+using System.IO;
+using System.Xml;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ThoughtWorks.CruiseControl.Core.Publishers
+{
+    [ReflectorType("xmllogger")]
+    public class XmlLogPublisher : ITask
+    {
+		public static readonly string DEFAULT_LOG_SUBDIRECTORY = "buildlogs";
+
+        [ReflectorProperty("logDir", Required = false)] 
+		public string ConfiguredLogDirectory;
+
+		// This is only public because of a nasty hack which I (MR) put in the code. To be made private later...
+		public string LogDirectory(string artifactDirectory)
+		{
+			if (StringUtil.IsBlank(ConfiguredLogDirectory))
+			{
+				return Path.Combine(artifactDirectory, DEFAULT_LOG_SUBDIRECTORY);
+			}
+			else if (Path.IsPathRooted(ConfiguredLogDirectory))
+			{
+				return ConfiguredLogDirectory;
+			}
+			else
+			{
+				return Path.Combine(artifactDirectory, ConfiguredLogDirectory);
+			}
+		}
+
+		public void Run(IIntegrationResult result)
+		{
+			// only deal with known integration status
+			if (result.Status == IntegrationStatus.Unknown)
+				return;
+
+			string LogFileName = this.GetFilename(result);
+			string LogDirectory = this.LogDirectory(result.ArtifactDirectory);
+			result.AddIntegrationProperty("CCNetLogFilePath", Path.Combine(LogDirectory, LogFileName));
+			using (XmlIntegrationResultWriter integrationWriter = new XmlIntegrationResultWriter(CreateWriter(LogDirectory, LogFileName)))
+			{
+				integrationWriter.Formatting = Formatting.Indented;
+				integrationWriter.Write(result);
+			}
+		}
+
+        private TextWriter CreateWriter(string dirname, string filename)
+        {
+            // create directory if necessary
+            if (!Directory.Exists(dirname))
+                Directory.CreateDirectory(dirname);
+
+            string path = Path.Combine(dirname, filename);
+
+			// create XmlWriter using UTF8 encoding
+			return new StreamWriter(path);
+        }
+
+        private string GetFilename(IIntegrationResult result)
+        {
+			return new LogFile(result).Filename;
+        }
+    }
+}
