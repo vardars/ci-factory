1d78d3bd75649e0134660d7cdb0812530de27df5
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-01-02 00:12:27 +0000

    Modified by build script in build 1.0.0.49

--- Current/Product/ProjectInfo.vb
+++ Current/Product/ProjectInfo.vb
@@ -3,5 +3,5 @@ Imports System.Reflection
 Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyProduct("CI Factory")> 
-<Assembly: AssemblyInformationalVersion("1.0.0.48")> 
+<Assembly: AssemblyInformationalVersion("1.0.0.49")> 
 <Assembly: AssemblyCompany("http://www.cifactory.org")> 
