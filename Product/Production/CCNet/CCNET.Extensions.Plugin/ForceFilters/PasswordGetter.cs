080031ff3938fb8d1dfb632bbfd54cf88caa65eb
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-01-07 22:31:20 +0000

    CCNET build 1.0.0.58

---
+++ Product/Production/CCNet/CCNET.Extensions.Plugin/ForceFilters/PasswordGetter.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Specialized;
+using System.DirectoryServices;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+using CCNET.Extensions.ForceFilters.UserFilter;
+using System.Web;
+using CCNET.Extensions.Plugin.ForceFilters.PasswordFilter;
+using System.Windows.Forms;
+
+namespace CCNET.Extensions.Plugin.ForceFilters
+{
+    [Serializable]
+    public class PasswordGetter : IPasswordHelper
+    {
+
+        #region IPasswordHelper Members
+
+        public string GetPassword()
+        {
+            if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Request.Params["password"];
+            }
+            else
+            {
+                PasswordForm Questionare = new PasswordFilter.PasswordForm();
+                Questionare.ShowDialog();
+                return Questionare.Password;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Product/Production/Nant/NAnt.Contrib.Tasks/Functions/PerforceFunctions.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using NAnt.Core;
+using NAnt.Core.Types;
+using NAnt.Core.Attributes;
+using NAnt.Contrib.Tasks.Perforce;
+
+using Helper = NAnt.Contrib.Tasks.Perforce.Perforce;
+
+namespace NAnt.Contrib.Functions.Perforce
+{
+    [FunctionSet("perforce", "sourcecontrol")]
+    public class PerforceFunctions : FunctionSetBase
+    {
+        public PerforceFunctions(Project project, PropertyDictionary properties)
+            : base(project, properties)
+        {
+
+        }
+
+        [Function("workspace-exists")]
+        public bool WorkspaceExists(string client, string port)
+        {
+            string output = Helper.getProcessOutput("p4", string.Format("-p {0} clients", port), null);
+            string[] lines = output.Split('\n');
+            foreach (string line in lines)
+            {
+                string[] s2 = line.Split(' ');   // poor manz regex
+                if (s2.Length > 1)
+                {
+                    if (s2[1].Trim('\r') == client)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        [Function("get-changelist-number")]
+        public string GetChangeListNumber(string changelistName)
+        {
+            return Helper.GetChangelistNumber(Helper.GetUserName(), Helper.GetClient(), changelistName, true);
+        }
+    }
+}
