eee0fe87ed095ce446ba3e11d01b77d72d9ace41
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-01-07 22:31:20 +0000

    CCNET build 1.0.0.58

---
+++ tags/1.0.0.58/Product/Production/CCNet/core/IntegrationResultManager.cs
@@ -0,0 +1,72 @@
+using System;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ThoughtWorks.CruiseControl.Core
+{
+	public class IntegrationResultManager : IIntegrationResultManager
+	{
+		private readonly Project project;
+		private IIntegrationResult lastResult;
+		private IntegrationResult currentResult;
+
+		public IntegrationResultManager(Project project)
+		{
+			this.project = project;
+		}
+
+		public IIntegrationResult LastIntegrationResult
+		{
+			get
+			{
+				if (lastResult == null)
+				{
+					lastResult = LoadLastIntegration();
+				}
+				return lastResult;
+			}
+		}
+
+		private IIntegrationResult LoadLastIntegration()
+		{
+			IIntegrationResult result = project.StateManager.LoadState(project.Name);
+			result.WorkingDirectory = project.WorkingDirectory;
+			return result;
+		}
+
+		public IIntegrationResult StartNewIntegration()
+		{
+			currentResult = new IntegrationResult(project.Name, project.WorkingDirectory);
+			currentResult.LastIntegrationStatus = LastIntegrationResult.Status;
+			currentResult.BuildCondition = DetermineBuildCondition(BuildCondition.NoBuild);
+			currentResult.Label = project.Labeller.Generate(LastIntegrationResult);
+			currentResult.ArtifactDirectory = project.ArtifactDirectory;
+			currentResult.ProjectUrl = project.WebURL;
+			currentResult.LastSuccessfulIntegrationLabel = LastIntegrationResult.LastSuccessfulIntegrationLabel;
+			return currentResult;
+		}
+
+		private BuildCondition DetermineBuildCondition(BuildCondition buildCondition)
+		{
+			if (LastIntegrationResult.IsInitial())
+			{
+				return BuildCondition.ForceBuild;
+			}
+			return buildCondition;
+		}
+
+		public void FinishIntegration()
+		{
+			try
+			{
+				project.StateManager.SaveState(currentResult);
+			}
+			catch (Exception ex)
+			{
+				// swallow exception???
+				Log.Error("Unable to save integration result: " + ex.ToString());
+			}
+			lastResult = currentResult;
+		}
+	}
+}
---
+++ tags/1.0.0.58/Product/Production/Nant/CIFactory.NAnt.NCover/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("NCoverExplorer.NAntTasks")]
+[assembly: AssemblyDescription("NAnt tasks for executing NCover and NCoverExplorer.Console in continuous build processes.")]
+[assembly: AssemblyCompany("Kiwi Development Ltd")]
+[assembly: AssemblyCopyright("Copyright © 2006 Kiwi Development Ltd.")]
+[assembly: AssemblyProduct("NCoverExplorer NAnt Tasks")]
+[assembly: AssemblyVersion("1.3.5.1950")]
