c53352793602c4e8d3838ae5cc54b39568dafecd
Jay.Flowers <Jay.Flowers@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-01-13 21:25:12 +0000

    Fix: Try to preserve email users for ccnet project config.  This works for the default config.

--- Current/Product/Production/Nant/CIFactory.NAnt.UnitTest.Tasks/Tasks/AssertEqualTask.cs
+++ Current/Product/Production/Nant/CIFactory.NAnt.UnitTest.Tasks/Tasks/AssertEqualTask.cs
@@ -85,4 +85,198 @@ namespace CIFactory.NAnt.UnitTest.Tasks
         #endregion
 
     }
+
+    [TaskName("assertnotequal")]
+    public class AssertNotEqualTask : Task
+    {
+        #region Fields
+
+        private string _Actual = String.Empty;
+
+        private string _Expected = String.Empty;
+
+        private string _Message = String.Empty;
+        public string Variable;
+
+        #endregion
+
+        #region Properties
+
+        [TaskAttribute("actual", Required = true)]
+        public string Actual
+        {
+            get
+            {
+                return _Actual;
+            }
+            set
+            {
+                _Actual = value;
+            }
+        }
+
+        [TaskAttribute("expected", Required = true)]
+        public string Expected
+        {
+            get
+            {
+                return _Expected;
+            }
+            set
+            {
+                _Expected = value;
+            }
+        }
+
+        [TaskAttribute("message", Required = true)]
+        public string Message
+        {
+            get
+            {
+                return _Message;
+            }
+            set
+            {
+                _Message = value;
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void ExecuteTask()
+        {
+            try
+            {
+                Assert.AreNotEqual(this.Expected, this.Actual, this.Message, this.Expected, this.Actual);
+            }
+            catch (Exception ex)
+            {
+                throw new BuildException(ex.Message, this.Location, ex);
+            }
+        }
+
+        #endregion
+
+    }
+
+
+    [TaskName("assertgreaterthan")]
+    public class AssertGreaterThanTask : Task
+    {
+        #region Fields
+
+        private string _Right = String.Empty;
+
+        private string _Left = String.Empty;
+
+        private string _Message = String.Empty;
+
+        #endregion
+
+        #region Properties
+
+        [TaskAttribute("right", Required = true)]
+        public string Right
+        {
+            get
+            {
+                return _Right;
+            }
+            set
+            {
+                _Right = value;
+            }
+        }
+
+        [TaskAttribute("left", Required = true)]
+        public string Left
+        {
+            get
+            {
+                return _Left;
+            }
+            set
+            {
+                _Left = value;
+            }
+        }
+
+        [TaskAttribute("message", Required = true)]
+        public string Message
+        {
+            get
+            {
+                return _Message;
+            }
+            set
+            {
+                _Message = value;
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void ExecuteTask()
+        {
+            try
+            {
+                Assert.GreaterThan(this.Left, this.Right, this.Message, this.Left, this.Right);
+            }
+            catch (Exception ex)
+            {
+                throw new BuildException(ex.Message, this.Location, ex);
+            }
+        }
+
+        #endregion
+
+    }
+
+    [TaskName("assertfail")]
+    public class AssertFail : Task
+    {
+        #region Fields
+
+        private string _Message = String.Empty;
+
+        #endregion
+
+        #region Properties
+
+        [TaskAttribute("message", Required = true)]
+        public string Message
+        {
+            get
+            {
+                return _Message;
+            }
+            set
+            {
+                _Message = value;
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void ExecuteTask()
+        {
+            try
+            {
+                Assert.Fail(this.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new BuildException(ex.Message, this.Location, ex);
+            }
+        }
+
+        #endregion
+
+    }
 }
