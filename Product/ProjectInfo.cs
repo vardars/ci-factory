d1a6fe75dab21348d251f719abec14bcab5ac2ea
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-02-14 14:50:26 +0000

    CCNET build 1.1.0.2

---
+++ Product/ProjectInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("CI Factory")]
+[assembly: AssemblyInformationalVersion("1.1.0.1")]
+[assembly: AssemblyCompany("http://www.cifactory.org")]
