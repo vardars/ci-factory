6ae5bef60388767cb3f538eeb29f6555dd6dbec3
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-02-17 19:46:53 +0000

    CCNET build 1.0.1.3

---
+++ Product/Integration Tests/WatiNTests/Resources/Product/Unit Test/GuestBook.Test/Register.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading;
+using WatiN.Core;
+using WatiN.Core.DialogHandlers;
+using WatiN.Core.Exceptions;
+using WatiN.Core.Interfaces;
+using WatiN.Core.Logging;
+using MbUnit.Framework;
+using SHDocVw;
+
+namespace TestSpace
+{
+    [TestFixture(ApartmentState = ApartmentState.STA)]
+    public class TestClass
+    {
+
+        private IE _Ie;
+        public IE Ie
+        {
+            get
+            {
+                return _Ie;
+            }
+            set
+            {
+                _Ie = value;
+            }
+        }
+
+        [Test]
+        public void Register()
+        {
+            try
+            {
+                Ie = new IE("about:blank");
+                Ie.GoTo("http://localhost/GuestBook/GuestBook.aspx");
+                Ie.TextField(Find.ByName("name")).TypeText("Jay");
+                Ie.TextField(Find.ByName("comments")).TypeText("Hello");
+                Ie.Button(Find.ByName("save")).Click();
+
+                Assert.AreEqual("Jay", Ie.TableCell(Find.By("innerText", "Jay")).Text, @"innerText does not match");
+                Assert.AreEqual("Hello", Ie.TableCell(Find.By("innerText", "Hello")).Text, @"innerText does not match");
+            }
+            finally
+            {
+                if (Ie != null)
+                {
+                    InternetExplorer internetExplorer = (InternetExplorer)Ie.InternetExplorer;
+                    internetExplorer.Quit();
+                }
+            }
+        }
+    }
+}
+
