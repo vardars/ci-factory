004992c6b28149e66125d57c85fe2115faec78f0
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-02-18 01:24:05 +0000

    CCNET build 1.0.1.4

---
+++ tags/1.0.1.4/Product/Production/CCNet/WebDashboard/MVC/RequestController.cs
@@ -0,0 +1,20 @@
+namespace ThoughtWorks.CruiseControl.WebDashboard.MVC
+{
+	public class RequestController
+	{
+		private readonly IRequest request;
+		private readonly IActionFactory actionFactory;
+
+		public RequestController(IActionFactory actionFactory, IRequest request)
+		{
+			this.actionFactory = actionFactory;
+			this.request = request;
+		}
+
+		public IResponse Do()
+		{
+			IAction action = actionFactory.Create(request);
+			return action.Execute(request);
+		}
+	}
+}
---
+++ tags/1.0.1.4/Product/ProjectInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("CI Factory")]
+[assembly: AssemblyInformationalVersion("1.0.1.3")]
+[assembly: AssemblyCompany("http://www.cifactory.org")]
