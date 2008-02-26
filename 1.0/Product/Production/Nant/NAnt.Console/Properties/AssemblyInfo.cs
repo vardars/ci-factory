5c9af20bcb27dc9f8fa280c3b835699d58222b4e
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-02-26 20:30:30 +0000

    Modified by build script in build 1.0.1.5

--- 1.0/Product/Production/Nant/NAnt.Console/Properties/AssemblyInfo.cs
+++ 1.0/Product/Production/Nant/NAnt.Console/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Reflection;
 [assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile(@"..\NAnt.key")]
 [assembly: AssemblyKeyName("")]
-[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
 
