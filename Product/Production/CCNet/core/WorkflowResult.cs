44ad3f11efeeebe5c72bbf836cb62cadbbcbcc61
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-03-18 20:23:43 +0000

    CCNET build 1.1.0.4

---
+++ Product/Production/CCNet/core/WorkflowResult.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ThoughtWorks.CruiseControl.Core
+{
+	public class WorkflowResult : IntegrationResult
+	{
+		private ArrayList modifications = new ArrayList();
+
+		public override Modification[] Modifications
+		{
+			get { return (Modification[])modifications.ToArray(typeof(Modification)); }
+			set { modifications.AddRange(value); }
+		}
+	}
+}
