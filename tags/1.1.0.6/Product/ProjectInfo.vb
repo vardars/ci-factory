885ccc9098ca2a34430e93cf388e9fe0b9974690
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-06-24 14:24:13 +0000

    CCNET build 1.1.0.6

---
+++ tags/1.1.0.6/Product/ProjectInfo.vb
@@ -0,0 +1,7 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyProduct("CI Factory")> 
+<Assembly: AssemblyInformationalVersion("1.1.0.5")> 
+<Assembly: AssemblyCompany("http://www.cifactory.org")> 
