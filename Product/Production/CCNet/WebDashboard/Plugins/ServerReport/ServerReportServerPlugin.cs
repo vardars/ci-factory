77fd4c942ad5af03d2ec4cbc46ec1aac29ab9951
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-08-05 18:12:04 +0000

    CCNET build 1.1.0.15

---
+++ Product/Production/CCNet/WebDashboard/Plugins/ServerReport/ServerReportServerPlugin.cs
@@ -0,0 +1,54 @@
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.WebDashboard.Dashboard;
+using ThoughtWorks.CruiseControl.WebDashboard.IO;
+using ThoughtWorks.CruiseControl.WebDashboard.MVC;
+using ThoughtWorks.CruiseControl.WebDashboard.MVC.Cruise;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.Plugins.ServerReport
+{
+	// ToDo - Test!
+	[ReflectorType("serverReportServerPlugin")]
+	public class ServerReportServerPlugin : ICruiseAction, IPlugin
+	{
+		public static readonly string ACTION_NAME = "ViewServerReport";
+
+		private readonly IProjectGridAction projectGridAction;
+
+		public ServerReportServerPlugin(IProjectGridAction projectGridAction)
+		{
+			this.projectGridAction = projectGridAction;
+		}
+
+		public IResponse Execute(ICruiseRequest request)
+		{
+			return projectGridAction.Execute(ACTION_NAME, request.ServerSpecifier, request.Request);
+		}
+
+		public string LinkDescription
+		{
+			get { return "Server Report"; }
+		}
+
+		public INamedAction[] NamedActions
+		{
+			get {  return new INamedAction[] { new ImmutableNamedAction(ACTION_NAME, this) }; }
+		}
+
+        #region IPlugin Members
+
+
+        string IPlugin.ImageFileName
+        {
+            get
+            {
+                throw new System.Exception("The method or operation is not implemented.");
+            }
+            set
+            {
+                throw new System.Exception("The method or operation is not implemented.");
+            }
+        }
+
+        #endregion
+    }
+}
