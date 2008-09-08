3c90d349a563f6814f13281e55472bdd6f2e46b8
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-09-08 14:09:01 +0000

    Modified by build script in build 1.1.0.16

--- Current/Product/Production/Nant/CIFactory.NAnt.TargetProcess.Tasks/Properties/AssemblyInfo.cs
+++ Current/Product/Production/Nant/CIFactory.NAnt.TargetProcess.Tasks/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CIFactory.TargetProcess.NAnt.Tasks")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyCopyright("Copyright �  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
