5ba3db4713430193f0d7df4828280a9c96b8e6c8
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-11-04 19:40:36 +0000

    Modified by build script in build 1.1.0.25

--- Current/Product/ProjectInfo.cs
+++ Current/Product/ProjectInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("CI Factory")]
-[assembly: AssemblyInformationalVersion("1.1.0.23")]
+[assembly: AssemblyInformationalVersion("1.1.0.25")]
 [assembly: AssemblyCompany("http://www.cifactory.org")]
