3aba88950710555ed104f69db8301627349ed439
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-11-07 14:33:14 +0000

    CCNET build 1.1.0.30

---
+++ tags/1.1.0.30/Product/Production/CCNet/WebDashboard/MVC/ASPNET/HttpHandler.cs
@@ -0,0 +1,50 @@
+using System.Web;
+using ObjectWizard;
+using ThoughtWorks.CruiseControl.WebDashboard.Dashboard;
+
+namespace ThoughtWorks.CruiseControl.WebDashboard.MVC.ASPNET
+{
+	// No need for session state yet, but if we do later then we should also add IRequiresSessionState to list of interfaces
+	public class HttpHandler : IHttpHandler
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			DoSecurityChecks(context);
+
+			// ToDo - more on MimeTypes?
+			if (context.Request.FilePath.EndsWith(".xml"))
+			{
+				// ToDo - if we are specifying XML, shouldn't we throw valid XML exceptions?
+				context.Response.ContentType = "Text/XML";
+			}
+
+			ObjectGiver objectGiver = new CruiseObjectGiverInitializer(new ManagableObjectGiver()).SetupObjectGiverForRequest(context);
+			IResponse response = ((RequestController) objectGiver.GiveObjectByType(typeof(RequestController))).Do();
+
+			if (response is RedirectResponse)
+			{
+				context.Response.Redirect(((RedirectResponse) response).Url);
+			}
+			else
+			{
+				context.Response.Write(((HtmlFragmentResponse) response).ResponseFragment);
+			}
+
+			context.Response.Flush();
+		}
+
+		public bool IsReusable
+		{
+			get { return true; }
+		}
+
+		private void DoSecurityChecks(HttpContext context)
+		{
+			// Security Fix - see http://www.microsoft.com/security/incident/aspnet.mspx
+			if (context.Request.Path.IndexOf('\\') >= 0 || System.IO.Path.GetFullPath(context.Request.PhysicalPath) != context.Request.PhysicalPath)
+			{
+				throw new HttpException(404, "not found");
+			}
+		}
+	}
+}
