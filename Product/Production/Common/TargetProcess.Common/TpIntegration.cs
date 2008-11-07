6cf809ba66858c33abde45e549304bb079f2aab4
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2008-11-07 14:33:14 +0000

    CCNET build 1.1.0.30

---
+++ Product/Production/Common/TargetProcess.Common/TpIntegration.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.Web.Services3;
+using Microsoft.Web.Services3.Design;
+using Microsoft.Web.Services3.Security;
+using Microsoft.Web.Services3.Security.Tokens;
+
+
+namespace CIFactory.TargetProcess.Common
+{
+   public class TpPolicy : Policy
+   {
+      public TpPolicy()
+      {
+         Assertions.Add(new UsernameOverTransportAssertion());
+      }
+
+      public static UsernameToken GetUsernameToken(string username, string password, PasswordOption passwordOption)
+      {
+         UsernameToken token = new UsernameToken(username, password, passwordOption);
+
+         ISecurityTokenManager securityTokenManager =
+            SecurityTokenManager.GetSecurityTokenManagerByTokenType(WSTrust.TokenTypes.UsernameToken);
+         securityTokenManager.CacheSecurityToken(token);
+
+         return token;
+      }
+
+      public static void ApplyAutheticationTicket(WebServicesClientProtocol protocol, string userName, string password)
+      {
+         UsernameToken token = GetUsernameToken(userName, password, PasswordOption.SendPlainText);
+         protocol.SetClientCredential(token);
+         protocol.SetPolicy(new TpPolicy());
+      }
+   }
+}
+
