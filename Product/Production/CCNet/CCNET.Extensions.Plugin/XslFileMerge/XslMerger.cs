d54c560bc0e9082a4472ee98cb8da2bf01c1e20d
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2009-03-20 19:49:49 +0000

    CCNET build 1.1.0.38

---
+++ Product/Production/CCNet/CCNET.Extensions.Plugin/XslFileMerge/XslMerger.cs
@@ -0,0 +1,100 @@
+using System;
+using System.IO;
+using System.Collections;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Core.Tasks;
+using System.Collections.Generic;
+
+namespace CCNET.Extensions.XslFileMerge
+{
+    [ReflectorType("xslmerger")]
+    public class XslMerger : ITask
+    {
+
+        private FilePairList _FilePairs;
+
+        [ReflectorCollection("filepairs", InstanceType = typeof(FilePairList), Required = true)]
+        public FilePairList FilePairs
+        {
+            get
+            {
+                return _FilePairs;
+            }
+            set
+            {
+                _FilePairs = value;
+            }
+        }
+
+        private string _DashboardServerName;
+        [ReflectorProperty("dashboardServerName")]
+        public string DashboardServerName
+        {
+            get
+            {
+                return _DashboardServerName;
+            }
+            set
+            {
+                _DashboardServerName = value;
+            }
+        }
+
+        public void Run(IIntegrationResult result)
+        {
+            foreach (FilePair Pair in this.FilePairs)
+            {
+                string XmlFilePath = Pair.XmlFile;
+                if (!Path.IsPathRooted(XmlFilePath))
+                {
+                    XmlFilePath = Path.Combine(result.WorkingDirectory, XmlFilePath);
+                }
+
+                string XslFilePath = Pair.XslFile;
+                if (!Path.IsPathRooted(XslFilePath))
+                {
+                    XslFilePath = Path.Combine(result.WorkingDirectory, XslFilePath);
+                }
+
+                string XslFileName = Path.GetFileName(XslFilePath);
+                if (!File.Exists(XslFilePath))
+                {
+                    Log.Warning("File not Found: " + XslFileName);
+                }
+
+                WildCardPath Pattern = new WildCardPath(XmlFilePath);
+                FileInfo[] Files = Pattern.GetFiles();
+                foreach (FileInfo XmlFileInfo in Files)
+                {
+                    Log.Info(String.Format("Merging file {0} through {1}", XmlFileInfo, XslFileName));
+                    if (XmlFileInfo.Exists)
+                    {
+                        string Data;
+                        String Contents;
+                        using (TextReader Reader = XmlFileInfo.OpenText())
+                        {
+                            Contents = Reader.ReadToEnd();
+                        }
+                        XslTransformer Transformer = new XslTransformer();
+                        
+                        Dictionary<string, string> XslParms = new Dictionary<string, string>();
+                        XslParms.Add("CCNetServer", this.DashboardServerName);
+                        XslParms.Add("CCNetBuild", new LogFile(result).Filename);
+                        XslParms.Add("CCNetProject", result.ProjectName);
+                        XslParms.Add("XmlSourceName", Path.GetFileName(XmlFilePath));
+                        XslParms.Add("XmlSourcePath", XmlFileInfo.FullName);
+
+                        Data = Transformer.Transform(Contents, XslFilePath, XslParms);
+                        result.AddTaskResult((new XslMergerTaskResult(Data)));
+                    }
+                    else
+                    {
+                        Log.Warning("File not Found: " + XmlFileInfo);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Product/Production/Nant/CIFactory.NAnt.TargetProcess.Tasks/Functions/TargetProcessFunctions.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NAnt.Core;
+using NAnt.Core.Types;
+using NAnt.Core.Attributes;
+using CIFactory.TargetProcess.NAnt.Helpers;
+using CIFactory.TargetProcess.NAnt.DataTypes;
+using CIFactory.TargetProcess.Common.IterationWebService;
+using CIFactory.TargetProcess.Common.ProjectWebService;
+
+namespace CIFactory.TargetProcess.NAnt.Functions
+{
+    [FunctionSet("tp", "target process")]
+    public class TargetProcessFunctions : FunctionSetBase
+    {
+        
+        #region Constructors
+
+        public TargetProcessFunctions(Project project, PropertyDictionary properties)
+            : base(project, properties)
+        {
+        }
+
+        public TargetProcessFunctions()
+            : base(null, null)
+        {
+        }
+
+        #endregion
+
+        private static IterationDTO GetCurrentIteration(int projectId, ConnectionInformation ConnectionInformation)
+        {
+            ServicesCF.ConnectionInformation = ConnectionInformation;
+            IterationService iterationService = ServicesCF.GetService<IterationService>();
+
+            IterationDTO[] iterations = iterationService.RetrieveAll();
+
+            List<IterationDTO> currentIterationList = iterations.Where(iteration => iteration.ParentProjectID == projectId && iteration.StartDate <= DateTime.Today && iteration.EndDate >= DateTime.Today).ToList<IterationDTO>();
+
+            if (currentIterationList.Count == 0)
+                throw new BuildException("Could not find the current iteration.");
+
+            IterationDTO currentIteration = currentIterationList[0];
+            return currentIteration;
+        }
+        
+        private static int FindProjectId(string projectName)
+        {
+            ProjectService projectService = ServicesCF.GetService<ProjectService>();
+
+            string hqlQuery = "select from Project as project where project.Name = ?";
+            ProjectDTO[] projects = projectService.Retrieve(hqlQuery, new object[] { projectName });
+
+            if (projects.Length == 0)
+                throw new BuildException(string.Format("Could not find a project named: '{0}'.", projectName));
+
+            return projects[0].ProjectID.Value;
+        }
+
+        [Function("get-current-iteration-id")]
+        public int GetCurrentIterationId(String connectionRef, string projectName)
+        {
+            if (!this.Project.DataTypeReferences.Contains(connectionRef))
+                throw new BuildException(String.Format("The refid {0} is not defined.", connectionRef));
+
+            ConnectionInformation ConnectionInformation = (ConnectionInformation)this.Project.DataTypeReferences[connectionRef];
+
+            IterationDTO currentIteration = GetCurrentIteration(FindProjectId(projectName), ConnectionInformation);
+
+            return currentIteration.ID.Value;
+        }
+
+        [Function("get-current-iteration-name")]
+        public string GetCurrentIterationName(String connectionRef, string projectName)
+        {
+            if (!this.Project.DataTypeReferences.Contains(connectionRef))
+                throw new BuildException(String.Format("The refid {0} is not defined.", connectionRef));
+
+            ConnectionInformation ConnectionInformation = (ConnectionInformation)this.Project.DataTypeReferences[connectionRef];
+
+            IterationDTO currentIteration = GetCurrentIteration(FindProjectId(projectName), ConnectionInformation);
+
+            return currentIteration.Name;
+        }
+
+        [Function("get-current-iteration-start-date")]
+        public DateTime GetCurrentIterationStartDate(String connectionRef, string projectName)
+        {
+            if (!this.Project.DataTypeReferences.Contains(connectionRef))
+                throw new BuildException(String.Format("The refid {0} is not defined.", connectionRef));
+
+            ConnectionInformation ConnectionInformation = (ConnectionInformation)this.Project.DataTypeReferences[connectionRef];
+
+            IterationDTO currentIteration = GetCurrentIteration(FindProjectId(projectName), ConnectionInformation);
+
+            return currentIteration.StartDate.Value;
+        }
+
+        [Function("get-current-iteration-end-date")]
+        public DateTime GetCurrentIterationEndDate(String connectionRef, string projectName)
+        {
+            if (!this.Project.DataTypeReferences.Contains(connectionRef))
+                throw new BuildException(String.Format("The refid {0} is not defined.", connectionRef));
+
+            ConnectionInformation ConnectionInformation = (ConnectionInformation)this.Project.DataTypeReferences[connectionRef];
+
+            IterationDTO currentIteration = GetCurrentIteration(FindProjectId(projectName), ConnectionInformation);
+
+            return currentIteration.EndDate.Value;
+        }
+    }
+}
