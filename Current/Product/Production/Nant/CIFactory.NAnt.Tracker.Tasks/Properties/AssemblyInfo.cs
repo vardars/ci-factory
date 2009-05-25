6b648fb6502051a204abcda8588c3aca1e6d6e46
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2009-05-25 13:36:07 +0000

    Modified by build script in build 1.1.0.40

--- Current/Product/Production/Nant/CIFactory.NAnt.Tracker.Tasks/Properties/AssemblyInfo.cs
+++ Current/Product/Production/Nant/CIFactory.NAnt.Tracker.Tasks/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("nAnt tasks for manipulating PVCS Trackers")]
 [assembly: ComVisible(false)]
 [assembly: Guid("13e7f34b-09c6-4e64-992b-1cb875adfcc3")]
-[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
 
