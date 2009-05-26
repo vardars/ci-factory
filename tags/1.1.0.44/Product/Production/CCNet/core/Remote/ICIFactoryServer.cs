2e0efef2e448be77f6835b4988241d7b662390c5
cifactory <cifactory@571f4c53-cd24-0410-92e9-33dde47f2f63>
2009-05-26 18:09:24 +0000

    CCNET build 1.1.0.44

---
+++ tags/1.1.0.44/Product/Production/CCNet/core/Remote/ICIFactoryServer.cs
@@ -0,0 +1,102 @@
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using ThoughtWorks.CruiseControl.Core.Util;
+
+namespace ThoughtWorks.CruiseControl.Remote
+{
+    [ServiceContract]
+    public interface ICIFactoryServer
+    {
+
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetProjectProcessInformation/{projectName}"
+        )]
+        ProcessInformationList GetProjectProcessInformation(string projectName);
+
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetHostServerName/{projectName}"
+        )]
+        string GetHostServerName(string projectName);
+
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetProjectsStatus"
+        )]
+        ProjectStatus[] GetProjectsStatus();
+
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetProjectStatus/{projectName}"
+        )]
+        ProjectStatus GetProjectStatus(string projectName);
+
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetLatestBuildName/{projectName}"
+        )]
+        string GetLatestBuildName(string projectName);
+
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetBuildNames/{projectName}"
+        )]
+        string[] GetBuildNames(string projectName);
+
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetMostRecentBuildNames/{projectName}/{buildCount}"
+        )]
+        string[] GetMostRecentBuildNames(string projectName, string buildCount);
+
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetLog/{projectName}/{buildName}"
+        )]
+        string GetLog(string projectName, string buildName);
+
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetVersion"
+        )]
+        string GetVersion();
+
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Bare,
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetExternalLinks/{projectName}"
+        )]
+        ExternalLink[] GetExternalLinks(string projectName);
+    }
+}
