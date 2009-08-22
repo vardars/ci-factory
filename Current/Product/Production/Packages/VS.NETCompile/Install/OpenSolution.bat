4c42fc96602a7f7cd498fe27ea66bdf766c6e2b7
justin.webster <justin.webster@571f4c53-cd24-0410-92e9-33dde47f2f63>
2009-08-22 22:22:48 +0000

    [Backward-Compatibility Issue] [API Change] Changes related to the new naming conventions: 1. Changed "ProductDirectoryName" to "Common.Directory.Product.Name".

--- Current/Product/Production/Packages/VS.NETCompile/Install/OpenSolution.bat
+++ Current/Product/Production/Packages/VS.NETCompile/Install/OpenSolution.bat
@@ -6,7 +6,7 @@ set ENVBAT="%ProgramFiles%\Microsoft Visual Studio 8\VC\vcvarsall.bat"
 IF DEFINED ProgramFiles(x86) set ENVBAT="%ProgramFiles(x86)%\Microsoft Visual Studio 8\VC\vcvarsall.bat"
 call %ENVBAT%
 
-cd ..\..\%ProductDirectoryName%
+cd ..\..\%Common.Directory.Product.Name%
 nant -buildfile:OpenSolution.Script.xml
 FOR /F "TOKENS=1 DELIMS=," %%A IN ('dir /b *.sln') DO SET solutionfilename=%%A
 devenv "%solutionfilename%"
