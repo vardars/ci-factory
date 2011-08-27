829f9ea8f32e53cef695b693f5734a148dcd51bc
sergio.shen <sergio.shen@gmail.com>
2011-08-27 22:06:46 +0000

    Add a project to execute unit tests against new groovy related code

---
+++ Current/Product/Unit Test/NAnt/Test.CIFactory.NAnt.Groovy.Tasks/TestTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Test.CIFactory.NAnt.Groovy.Tasks
+{
+
+    [TestFixture]
+    public class TestTest
+    {
+        [Test]
+        public void bah(){
+            Assert.AreEqual(1.0, 2.0);
+        }
+
+    }
+}
